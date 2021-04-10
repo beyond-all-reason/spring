2567cf05d5196d573095f4d7897dd3e132d33f78
LHoG <1476261+lhog@users.noreply.github.com>
2021-04-10 03:04:26 +0300

    Allow matrix selection

--- cont/base/springcontent/shaders/GLSL/ModelVertProgGL4.glsl
+++ cont/base/springcontent/shaders/GLSL/ModelVertProgGL4.glsl
@@ -30,7 +30,6 @@ layout(std140, binding = 0) uniform UniformMatrixBuffer {
 	//TODO: minimap matrices
 };
 
-
 layout(std140, binding = 1) uniform UniformParamsBuffer {
 	vec3 rndVec3; //new every draw frame.
 	uint renderCaps; //various render booleans
@@ -38,7 +37,7 @@ layout(std140, binding = 1) uniform UniformParamsBuffer {
 	vec4 timeInfo; //gameFrame, gameSeconds, drawFrame, frameTimeOffset
 	vec4 viewGeometry; //vsx, vsy, vpx, vpy
 	vec4 mapSize; //xz, xzPO2
-	
+
 	vec4 fogColor; //fog color
 	vec4 fogParams; //fog {start, end, 0.0, scale}
 	vec4 teamColor[255];
@@ -47,6 +46,8 @@ layout(std140, binding = 1) uniform UniformParamsBuffer {
 layout(std140, binding=0) readonly buffer MatrixBuffer {
 	mat4 mat[];
 };
+
+uniform int cameraMode = 0;
 
 mat4 mat4mix(mat4 a, mat4 b, float alpha) {
 	return (a * (1.0 - alpha) + b * alpha);
@@ -61,13 +62,23 @@ void main(void)
 {
 	uint baseIndex = instData.x; //ssbo offset
 	mat4 modelMatrix = mat[baseIndex];
-	
+
 	mat4 pieceMatrix = mat4mix(mat4(1.0), mat[baseIndex + pieceIndex + 1u], modelMatrix[3][3]);
 
-	vec4 worldPos = modelMatrix * pieceMatrix * vec4(pos, 1.0);	
-	
+	vec4 worldPos = modelMatrix * pieceMatrix * vec4(pos, 1.0);
+
 	teamCol = teamColor[instData.y]; // team index
 	uvCoord = uv;
 
-	gl_Position = cameraViewProj * worldPos;	
+	mat4 VP;
+
+	switch(cameraMode) {
+		case 1:  VP = shadowViewProj; break; //shadow
+		case 2:  VP = mat4(1.0f);     break; //underwater
+		case 0:
+		default: VP = cameraViewProj; break; //player
+	};
+
+
+	gl_Position = VP * worldPos;
 }
