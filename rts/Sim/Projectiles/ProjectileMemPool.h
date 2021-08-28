5f4b5a869c76d3d205f76c9af0219b3bf359a18a
LHoG <1476261+lhog@users.noreply.github.com>
2021-08-28 17:35:51 +0300

    Fix hardcoded S of ProjectileMemPool's FixedDynMemPool/StaticMemPool

--- rts/Sim/Projectiles/ProjectileMemPool.h
+++ rts/Sim/Projectiles/ProjectileMemPool.h
@@ -6,10 +6,14 @@
 #include "Sim/Misc/GlobalConstants.h"
 #include "System/MemPoolTypes.h"
 
+#include "Sim/Projectiles/WeaponProjectiles/StarburstProjectile.h"
+
+static constexpr size_t PMP_S = sizeof(CStarburstProjectile); //biggest in size
+
 #if (defined(__x86_64) || defined(__x86_64__))
-typedef StaticMemPool<MAX_PROJECTILES, 868> ProjMemPool;
+typedef StaticMemPool<MAX_PROJECTILES, PMP_S> ProjMemPool;
 #else
-typedef FixedDynMemPool<868, MAX_PROJECTILES / 2000, MAX_PROJECTILES / 64> ProjMemPool;
+typedef FixedDynMemPool<PMP_S, MAX_PROJECTILES / 2000, MAX_PROJECTILES / 64> ProjMemPool;
 #endif
 
 extern ProjMemPool projMemPool;
