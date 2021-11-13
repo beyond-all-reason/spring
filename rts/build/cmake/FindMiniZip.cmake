1614dbeb596b538130d5e6698d035041354bdf19
verybadsoldier <vbs@springrts.de>
2021-11-13 23:36:00 +0000

    fix cmake warning: unify uppercase lowercase in MiniZip find module

--- rts/build/cmake/FindMiniZip.cmake
+++ rts/build/cmake/FindMiniZip.cmake
@@ -3,30 +3,30 @@
 # - Find the MiniZip library (sub-part of zlib)
 # Find the native MiniZip includes and library (static or shared)
 #
-#  MINIZIP_INCLUDE_DIR - where to find zip.h, unzip.h, ioapi.h, etc.
-#  MINIZIP_LIBRARIES   - List of libraries when using minizip.
-#  MINIZIP_FOUND       - True if minizip was found.
+#  MiniZip_INCLUDE_DIR - where to find zip.h, unzip.h, ioapi.h, etc.
+#  MiniZip_LIBRARIES   - List of libraries when using minizip.
+#  MiniZip_FOUND       - True if minizip was found.
 
 Include(FindPackageHandleStandardArgs)
 
-If     (MINIZIP_INCLUDE_DIR)
+If     (MiniZip_INCLUDE_DIR)
   # Already in cache, be silent
-  Set(MINIZIP_FIND_QUIETLY TRUE)
-EndIf  (MINIZIP_INCLUDE_DIR)
+  Set(MiniZip_FIND_QUIETLY TRUE)
+EndIf  (MiniZip_INCLUDE_DIR)
 
-Find_Path(MINIZIP_INCLUDE_DIR minizip/zip.h)
+Find_Path(MiniZip_INCLUDE_DIR minizip/zip.h)
 
-Set(MINIZIP_NAMES minizip)
-Find_Library(MINIZIP_LIBRARY NAMES ${MINIZIP_NAMES})
+Set(MiniZip_NAMES minizip)
+Find_Library(MiniZip_LIBRARY NAMES ${MiniZip_NAMES})
 
-# handle the QUIETLY and REQUIRED arguments and set MINIZIP_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set MiniZip_FOUND to TRUE if
 # all listed variables are TRUE
-Find_Package_Handle_Standard_Args(MINIZIP DEFAULT_MSG MINIZIP_LIBRARY MINIZIP_INCLUDE_DIR)
+Find_Package_Handle_Standard_Args(MiniZip DEFAULT_MSG MiniZip_LIBRARY MiniZip_INCLUDE_DIR)
 
-If     (MINIZIP_FOUND)
-  Set(MINIZIP_LIBRARIES ${MINIZIP_LIBRARY})
-Else   (MINIZIP_FOUND)
-  Set(MINIZIP_LIBRARIES)
-EndIf  (MINIZIP_FOUND)
+If     (MiniZip_FOUND)
+  Set(MiniZip_LIBRARIES ${MiniZip_LIBRARY})
+Else   (MiniZip_FOUND)
+  Set(MiniZip_LIBRARIES)
+EndIf  (MiniZip_FOUND)
 
-Mark_As_Advanced(MINIZIP_LIBRARY MINIZIP_INCLUDE_DIR)
+Mark_As_Advanced(MiniZip_LIBRARY MiniZip_INCLUDE_DIR)
