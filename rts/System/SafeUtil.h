674213ec71b102c108ad074d4276b1ac6961d459
LHoG <1476261+lhog@users.noreply.github.com>
2021-12-01 16:27:04 +0300

    Add spring::bit_cast

--- rts/System/SafeUtil.h
+++ rts/System/SafeUtil.h
@@ -99,6 +99,18 @@ namespace spring {
 #pragma GCC diagnostic pop
 #endif
 
+    template <class T2, class T1>
+    T2 bit_cast(T1 t1) {
+        static_assert(sizeof(T1) == sizeof(T2), "Types must match sizes");
+        static_assert(std::is_pod<T1>::value, "Requires POD input");
+        static_assert(std::is_pod<T2>::value, "Requires POD output");
+
+        T2 t2;
+        std::memcpy(std::addressof(t2), std::addressof(t1), sizeof(T1));
+        return t2;
+    }
+
+
 };
 
 #endif
