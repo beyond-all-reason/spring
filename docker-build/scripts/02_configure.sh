d26dc0a844e946a3d70a26d46c774215b20febf6
Marek Rusinowski <marekrusinowski@gmail.com>
2023-01-07 21:43:09 +0100

    Adapt pr-downloader cmake rewrite and switch to pkg-config for linux.
    The pkg-config based packages resoltion simplifies the configuration and
    allows for better automatic resolution of indirect dependencies of
    libraries needed for static linking.

--- docker-build/scripts/02_configure.sh
+++ docker-build/scripts/02_configure.sh
@@ -7,11 +7,8 @@ mkdir -p "${BUILD_DIR}/bin-dir"
 
 EXTRA_CMAKE_ARGS=()
 if [ "${PLATFORM}" == "linux-64" ]; then
-    WORKDIR=${LIBS_DIR}
-    LIBDIR=$WORKDIR/lib
-    INCLUDEDIR=$WORKDIR/include
-
     EXTRA_CMAKE_ARGS+=(
+        -DCMAKE_SYSTEM_PREFIX_PATH="${LIBS_DIR}"
         -DPREFER_STATIC_LIBS:BOOL=1
         -DCMAKE_USE_RELATIVE_PATHS:BOOL=1
         -DBINDIR:PATH=./
@@ -19,45 +16,9 @@ if [ "${PLATFORM}" == "linux-64" ]; then
         -DDATADIR:PATH=./
         -DMANDIR:PATH=share/man
         -DDOCDIR:PATH=doc
-        -DOGG_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DOGG_LIBRARY=${LIBDIR}/libogg.a
-        -DMINIZIP_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DMINIZIP_LIBRARY=${LIBDIR}/libminizip.a
-        -DLZMA_LIBRARY=${LIBDIR}/liblzma.a
-        -DLIBUUID_LIBRARY=${LIBDIR}/libuuid.a
-        -DVORBIS_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DVORBISENC_LIBRARY=${LIBDIR}/libvorbisenc.a
-        -DVORBISFILE_LIBRARY=${LIBDIR}/libvorbisfile.a
-        -DVORBIS_LIBRARY=${LIBDIR}/libvorbis.a
-        -DIL_IL_HEADER:PATH=${INCLUDEDIR}/IL/il.h
-        -DIL_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DIL_IL_LIBRARY:PATH=${LIBDIR}/libIL.a
-        -DIL_LIBRARIES:PATH=${LIBDIR}/libIL.a
-        -DILU_LIBRARIES:PATH=${LIBDIR}/libILU.a
-        -DGIF_LIBRARY:PATH=${LIBDIR}/libgif.a
-        -DGIF_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DPNG_PNG_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DPNG_LIBRARY_RELEASE:PATH=${LIBDIR}/libpng.a
-        -DJPEG_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DJPEG_LIBRARY:PATH=${LIBDIR}/libjpeg.a
-        -DTIFF_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DTIFF_LIBRARY_RELEASE:PATH=${LIBDIR}/libtiff.a
-        -DZLIB_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DZLIB_LIBRARY_RELEASE:PATH=${LIBDIR}/libz.a
-        -DGLEW_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DGLEW_LIBRARIES:PATH=${LIBDIR}/libGLEW.a
-        -DLIBUNWIND_INCLUDE_DIRS:PATH=${INCLUDEDIR}
-        -DLIBUNWIND_LIBRARY:PATH=${LIBDIR}/libunwind.a
-        -DCURL_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DCURL_LIBRARY:PATH="${LIBDIR}/libcurl.a;${LIBDIR}/libnghttp2.a"
-        -DOPENSSL_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DOPENSSL_SSL_LIBRARY:PATH=${LIBDIR}/libssl.a
-        -DOPENSSL_CRYPTO_LIBRARY:PATH=${LIBDIR}/libcrypto.a
-        -DVORBIS_INCLUDE_DIR:PATH=${INCLUDEDIR}
-        -DVORBISENC_LIBRARY:PATH=${LIBDIR}/libvorbisenc.a
-        -DVORBISFILE_LIBRARY:PATH=${LIBDIR}/libvorbisfile.a
-        -DVORBIS_LIBRARY:PATH=${LIBDIR}/libvorbis.a
     )
+    export PKG_CONFIG_LIBDIR=${LIBS_DIR}/lib/pkgconfig
+    export PKG_CONFIG="pkg-config --define-prefix --static"
 elif [ "${PLATFORM}" == "windows-64" ]; then
     EXTRA_CMAKE_ARGS+=(
         -DMINGWLIBS=${LIBS_DIR}
