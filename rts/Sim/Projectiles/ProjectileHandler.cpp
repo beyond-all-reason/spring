beyond-all-reason/spring

commit 83e08eeb5f05bbf936d75804d0a531ef0052c601

short-circuit a bit

rts/Sim/Projectiles/ProjectileHandler.cpp
@@ -426,12 +426,16 @@ void CProjectileHandler::AddProjectile(CProjectile* p)
 
 static bool CheckProjectileCollisionFlags(const CProjectile* p, const CUnit* u)
 {
-	const unsigned int collFlags = p->GetCollisionFlags();
+	const unsigned int collFlags = p->GetCollisionFlags() * p->weapon;
+
+	// only weapon-projectiles can have non-zero flags
+	if (collFlags == 0)
+		return false;
 
 	// disregard everything else when this bit is set
 	// (ground and feature flags are tested elsewhere)
 	if ((collFlags & Collision::NONONTARGETS) != 0)
-		return (p->weapon && static_cast<const CWeaponProjectile*>(p)->GetTargetObject() == u);
+		return (static_cast<const CWeaponProjectile*>(p)->GetTargetObject() == u);
 
 	if ((collFlags & Collision::NOCLOAKED) != 0 && u->IsCloaked())
 		return false;
@@ -454,7 +458,6 @@ static bool CheckProjectileCollisionFlags(const CProjectile* p, const CUnit* u)
 
 		if (noFriendsBit && friendlyFire)
 			return false;
-
 		if (noEnemiesBit && !friendlyFire)
 			return false;
 	}

commit 4671a074870468fce6f7b75c30b11d397c7842ec

Piece projectiles collide with units again
Fixes a bug introduced in https://github.com/beyond-all-reason/spring/commit/83e08eeb5f05bbf936d75804d0a531ef0052c601 (104.0.1-1182)

Non-weapon projectiles, which includes piece projectiles (explosive shards flying from dying units), have 0 flags.
So before that commit they just skipped all the conditions and went to the 'true' at the end of the function.
After that commit they short-circuit to 'false'. This means they only collide with features and ground (which are handled in other functions).

rts/Sim/Projectiles/ProjectileHandler.cpp
@@ -371,7 +371,7 @@ static bool CheckProjectileCollisionFlags(const CProjectile* p, const CUnit* u)
 
 	// only weapon-projectiles can have non-zero flags
 	if (collFlags == 0)
-		return false;
+		return true;
 
 	// disregard everything else when this bit is set
 	// (ground and feature flags are tested elsewhere)