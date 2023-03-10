85e72605e4587308f3c6cd5de8cb08714d3633b8
Amadeus Folego <amadeusfolego@gmail.com>
2023-03-09 21:08:45 -0300

    chore(site): Automatically fill engine data

--- doc/site/_scripts/get_release_data.sh
+++ doc/site/_scripts/get_release_data.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DATA_FILE="$SCRIPT_DIR/../_data/latest_release.json"
 
+echo "> writing $DATA_FILE"
+
 rm $DATA_FILE
 curl -s https://api.github.com/repos/beyond-all-reason/spring/releases/latest  \
   > $DATA_FILE
