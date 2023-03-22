7e4dc898c822ee2119f662423d35bbe1ae94d348
LHoG <1476261+lhog@users.noreply.github.com>
2023-03-22 17:33:24 +0400

    LuaMemPool update

--- rts/Lua/LuaMemPool.cpp
+++ rts/Lua/LuaMemPool.cpp
@@ -10,15 +10,7 @@
 #include "System/SafeUtil.h"
 #include "System/Log/ILog.h"
 #include "System/Threading/SpringThreading.h"
-
-// if 1, places an upper limit on pool allocation size
-// needed most when free chunks are stored in an array
-// rather than a hmap since the former would be highly
-// sparse; it also prevents the larger (ie more rarely
-// requested, so not often recycled either) allocations
-// from accumulating
-#define CHECK_MAX_ALLOC_SIZE 1
-
+#include "lib/fmt/printf.h"
 
 // global, affects all pool instances
 bool LuaMemPool::enabled = false;
@@ -31,12 +23,6 @@ static std::vector<size_t> gIndcs;
 static std::atomic<size_t> gCount = {0};
 static spring::mutex gMutex;
 
-
-// Lua code tends to perform many smaller *short-lived* allocations
-// this frees us from having to handle all possible sizes, just the
-// most common
-static bool AllocInternal(size_t size) { return ((size * CHECK_MAX_ALLOC_SIZE) <= LuaMemPool::MAX_ALLOC_SIZE); }
-static bool AllocExternal(size_t size) { return (!LuaMemPool::enabled || !AllocInternal(size)); }
 
 size_t LuaMemPool::GetPoolCount() { return (gCount.load()); }
 
@@ -60,13 +46,9 @@ LuaMemPool* LuaMemPool::AcquirePtr(bool shared, bool owned)
 		gMutex.unlock();
 	}
 
-	// only wipe statistics; blocks will be recycled
-	// p->ClearStats((p->GetSharedCount() += shared) <= 1);
-
 	// wipe statistics and blocks if we are the first to request p
 	if ((p->GetSharedCount() += shared) <= 1) {
 		p->Clear();
-		p->Reserve(16384);
 	}
 
 	// track the number of active state-owned pools (for /debug)
@@ -110,313 +92,122 @@ LuaMemPool::LuaMemPool(size_t lmpIndex): globalIndex(lmpIndex)
 	if (!LuaMemPool::enabled)
 		return;
 
-	poolImpl.Init();
-	Reserve(16384);
+	luaMemPoolImpl = std::make_unique<LuaMemPool::LuaMemPoolImpl>();
 }
 
-
-void LuaMemPool::LogStats(const char* handle, const char* lctype) const
+void LuaMemPool::Clear()
 {
-	#if (LMP_USE_CHUNK_TABLE == 1)
-		LOG(
-			"[LuaMemPool::%s][handle=%s (%s)] index=" _STPF_ " {blocks,sizes}={" _STPF_ "," _STPF_ "} {int,ext,rec}Allocs={" _STPF_ "," _STPF_ "," _STPF_ "} {chunk,block}Bytes={" _STPF_ "," _STPF_ "}",
-			__func__,
-			handle,
-			lctype,
-			globalIndex,
-			allocBlocks.size(),
-			chunkCountTable.size(),
-			allocStats[STAT_NIA],
-			allocStats[STAT_NEA],
-			allocStats[STAT_NRA],
-			allocStats[STAT_NCB],
-			allocStats[STAT_NBB]
-		);
-	#else
-		LOG(
-			"[LuaMemPool::%s][handle=%s (%s)] index=" _STPF_ " {numAllocs[*],allocSums[*]}={" _STPF_ "," _STPF_ "} {int,ext,rec}Allocs={" _STPF_ "," _STPF_ "," _STPF_ "} {chunk,block}Bytes={" _STPF_ "," _STPF_ "}",
-			__func__,
-			handle,
-			lctype,
-			globalIndex,
-			poolImpl.numAllocs[PoolImpl::NUM_POOLS],
-			poolImpl.allocSums[PoolImpl::NUM_POOLS],
-			allocStats[STAT_NIA],
-			allocStats[STAT_NEA],
-			allocStats[STAT_NRA],
-			allocStats[STAT_NCB],
-			allocStats[STAT_NBB]
-		);
-	#endif
-}
-
-
-void LuaMemPool::DeleteBlocks()
-{
-	#if (LMP_USE_CHUNK_TABLE == 1)
-	#if 1
-	for (void* p: allocBlocks) {
-		::operator delete(p);
-	}
-
-	allocBlocks.clear();
-	#endif
-	#endif
+	//allocStats = {};
 }
 
 void* LuaMemPool::Alloc(size_t size)
 {
-	if (AllocExternal(size)) {
-		allocStats[STAT_NEA] += 1;
-		return ::operator new(size);
-	}
-
-	allocStats[STAT_NIA] += 1;
-	allocStats[STAT_NCB] += (size = std::max(size, size_t(MIN_ALLOC_SIZE)));
-
-	#if (LMP_USE_CHUNK_TABLE == 1)
-	auto freeChunksTablePair = std::make_pair(freeChunksTable.find(size), false);
-
-	if (freeChunksTablePair.first == freeChunksTable.end())
-		freeChunksTablePair = freeChunksTable.insert(size, nullptr);
-
-	void* ptr = (freeChunksTablePair.first)->second;
-
-	if (ptr != nullptr) {
-		(freeChunksTablePair.first)->second = (*(void**) ptr);
-
-		allocStats[STAT_NRA] += 1;
+	if (!LuaMemPool::enabled) {
+		allocStats[STAT_NAE] += 1 * (size > 0);
+		allocStats[STAT_NBE] += size;
+		auto t0 = spring_now();
+		void* ptr = ::operator new(size);
+		allocStats[STAT_NTE] += (spring_now() - t0).toMicroSecsi();
 		return ptr;
 	}
 
-	auto chunkCountTablePair = std::make_pair(chunkCountTable.find(size), false);
-
-	if (chunkCountTablePair.first == chunkCountTable.end())
-		chunkCountTablePair = chunkCountTable.insert(size, 8);
-
-	const size_t numChunks = (chunkCountTablePair.first)->second;
-	const size_t numBytes = size * numChunks;
-
-	void* newBlock = ::operator new(numBytes);
-	uint8_t* newBytes = reinterpret_cast<uint8_t*>(newBlock);
-
-	#if 1
-	allocBlocks.push_back(newBlock);
-	#endif
-
-	// new allocation; construct chain of chunks within the memory block
-	// (this requires the block size to be at least MIN_ALLOC_SIZE bytes)
-	for (size_t i = 0; i < (numChunks - 1); ++i) {
-		*(void**) &newBytes[i * size] = (void*) &newBytes[(i + 1) * size];
+	auto t0 = spring_now();
+	auto* ptr = luaMemPoolImpl->allocMem(size);
+	
+	if (size > NUM_BUCKETS * BUCKET_STEP) {
+		allocStats[STAT_NAE] += 1 * (size > 0);
+		allocStats[STAT_NBE] += size;
+		allocStats[STAT_NTE] += (spring_now() - t0).toMicroSecsi();
+	} else if (luaMemPoolImpl->isAllocInternal(ptr)) {
+		allocStats[STAT_NAI] += 1 * (size > 0);
+		allocStats[STAT_NBI] += size;
+		allocStats[STAT_NTI] += (spring_now() - t0).toMicroSecsi();
+	} else {
+		allocStats[STAT_NAF] += 1 * (size > 0);
+		allocStats[STAT_NBF] += size;
+		allocStats[STAT_NTF] += (spring_now() - t0).toMicroSecsi();
 	}
 
-	*(void**) &newBytes[(numChunks - 1) * size] = nullptr;
-
-	freeChunksTable[size] = (*(void**) newBlock);
-	chunkCountTable[size] *= 2; // geometric increase
-
-	allocStats[STAT_NBB] += numBytes;
-	return newBlock;
-	#else
-	return (poolImpl.Alloc(size));
-	#endif
+	return ptr;
 }
 
 void* LuaMemPool::Realloc(void* ptr, size_t nsize, size_t osize)
 {
-	void* ret = Alloc(nsize);
+	if (ptr == nullptr || osize == 0)
+		return Alloc(nsize);
 
-	if (ptr == nullptr)
-		return ret;
+	if (!LuaMemPool::enabled) {
+		void* newPtr = ::operator new(nsize);
 
-	std::memcpy(ret, ptr, std::min(nsize, osize));
-	std::memset(ptr, 0, osize);
+		if (newPtr == nullptr)
+			return nullptr;
 
-	Free(ptr, osize);
+		allocStats[STAT_NBE] -= osize;
+		allocStats[STAT_NBE] += nsize;
+
+		auto t0 = spring_now();
+		std::memcpy(newPtr, ptr, std::min(nsize, osize));
+		std::memset(ptr, 0, osize);
+		::operator delete(ptr);
+		allocStats[STAT_NTE] += (spring_now() - t0).toMicroSecsi();
+
+		return newPtr;
+	}
+
+	auto t0 = spring_now();
+	auto* ret = luaMemPoolImpl->reAllocMem(ptr, nsize);
+	if (nsize > NUM_BUCKETS * BUCKET_STEP) {
+		allocStats[STAT_NAE] += 1 * (nsize > 0);
+		allocStats[STAT_NBE] += nsize;
+		allocStats[STAT_NTE] += (spring_now() - t0).toMicroSecsi();
+	}
+	else if (luaMemPoolImpl->isAllocInternal(ret)) {
+		allocStats[STAT_NAI] += 1 * (nsize > 0);
+		allocStats[STAT_NBI] += nsize;
+		allocStats[STAT_NTI] += (spring_now() - t0).toMicroSecsi();
+	}
+	else {
+		allocStats[STAT_NAF] += 1 * (nsize > 0);
+		allocStats[STAT_NBF] += nsize;
+		allocStats[STAT_NTF] += (spring_now() - t0).toMicroSecsi();
+	}
 	return ret;
 }
 
 void LuaMemPool::Free(void* ptr, size_t size)
 {
-	if (ptr == nullptr)
-		return;
-
-	if (AllocExternal(size)) {
-		::operator delete(ptr);
+	if (!LuaMemPool::enabled) {
+		spring::SafeDelete(ptr);
 		return;
 	}
 
-	allocStats[STAT_NCB] -= (size = std::max(size, size_t(MIN_ALLOC_SIZE)));
-
-	#if (LMP_USE_CHUNK_TABLE == 1)
-	*(void**) ptr = freeChunksTable[size];
-	freeChunksTable[size] = ptr;
-	#else
-	poolImpl.Free(ptr, size);
-	#endif
+	luaMemPoolImpl->freeMem(ptr);
 }
 
-
-
-
-void LuaMemPool::PoolImpl::Init() {
-	poolPtrs.fill(nullptr);
-	numAllocs.fill(0);
-	allocSums.fill(0);
-
-	poolPtrs[ 0] = NewPool< 0>();
-	poolPtrs[ 1] = NewPool< 1>();
-	poolPtrs[ 2] = NewPool< 2>();
-	poolPtrs[ 3] = NewPool< 3>();
-	poolPtrs[ 4] = NewPool< 4>();
-	poolPtrs[ 5] = NewPool< 5>();
-	poolPtrs[ 6] = NewPool< 6>();
-	poolPtrs[ 7] = NewPool< 7>();
-	poolPtrs[ 8] = NewPool< 8>();
-	poolPtrs[ 9] = NewPool< 9>();
-	poolPtrs[10] = NewPool<10>();
-	poolPtrs[11] = NewPool<11>();
-	poolPtrs[12] = NewPool<12>();
-	poolPtrs[13] = NewPool<13>();
-	poolPtrs[14] = NewPool<14>();
-	poolPtrs[15] = NewPool<15>();
-	poolPtrs[16] = NewPool<16>();
-	poolPtrs[17] = NewPool<17>();
-	poolPtrs[18] = NewPool<18>();
-	poolPtrs[19] = NewPool<19>();
-	poolPtrs[20] = NewPool<20>();
-	poolPtrs[21] = NewPool<21>();
-	poolPtrs[22] = NewPool<22>();
-	poolPtrs[23] = NewPool<23>();
-	poolPtrs[24] = NewPool<24>();
-	poolPtrs[25] = NewPool<25>();
-	poolPtrs[26] = NewPool<26>();
+void LuaMemPool::LogStats(const char* handle, const char* lctype)
+{
+	static constexpr auto one = uint64_t(1);
+	const float intPerc = 100.0f * static_cast<float>(allocStats[STAT_NAI]) / static_cast<float>(std::max(allocStats[STAT_NAI] + allocStats[STAT_NAF] + allocStats[STAT_NAE], one));
+	const float avgAllocTimeI = static_cast<float>(allocStats[STAT_NTI]) / static_cast<float>(std::max(allocStats[STAT_NAI], one));
+	const float avgAllocTimeF = static_cast<float>(allocStats[STAT_NTF]) / static_cast<float>(std::max(allocStats[STAT_NAF], one));
+	const float avgAllocTimeE = static_cast<float>(allocStats[STAT_NTE]) / static_cast<float>(std::max(allocStats[STAT_NAE], one));
+	std::string msg = fmt::sprintf(
+		"[LuaMemPool::%s][handle=%s (%s)] index=%u numAllocs{int+, int-, ext, int_p}={%u, %u, %u, %.1f} allocedSize{int+, int-, ext}={%u, %u, %u}, avgAllocTime{int+, int-, ext}={%.4f, %.4f, %.4f}",
+		__func__,
+		handle,
+		lctype,
+		globalIndex,
+		allocStats[STAT_NAI],
+		allocStats[STAT_NAF],
+		allocStats[STAT_NAE],
+		intPerc,
+		allocStats[STAT_NBI],
+		allocStats[STAT_NBF],
+		allocStats[STAT_NBE],
+		avgAllocTimeI,
+		avgAllocTimeF,
+		avgAllocTimeE
+	);
+	LOG(msg.c_str());
+	allocStats = {};
 }
-
-void LuaMemPool::PoolImpl::Kill() {
-	KillPool< 0>();
-	KillPool< 1>();
-	KillPool< 2>();
-	KillPool< 3>();
-	KillPool< 4>();
-	KillPool< 5>();
-	KillPool< 6>();
-	KillPool< 7>();
-	KillPool< 8>();
-	KillPool< 9>();
-	KillPool<10>();
-	KillPool<11>();
-	KillPool<12>();
-	KillPool<13>();
-	KillPool<14>();
-	KillPool<15>();
-	KillPool<16>();
-	KillPool<17>();
-	KillPool<18>();
-	KillPool<19>();
-	KillPool<20>();
-	KillPool<21>();
-	KillPool<22>();
-	KillPool<23>();
-	KillPool<24>();
-	KillPool<25>();
-	KillPool<26>();
-
-	poolPtrs.fill(nullptr);
-}
-
-
-void* LuaMemPool::PoolImpl::Alloc(uint32_t size) {
-	const uint32_t subPoolIndex = CalcPoolIndex(size);
-
-	numAllocs[subPoolIndex] += 1;
-	allocSums[subPoolIndex] += size;
-	numAllocs[   NUM_POOLS] += 1;
-	allocSums[   NUM_POOLS] += size;
-
-	switch (subPoolIndex) {
-		case  0: { return (GetPool< 0>()->allocMem(size)); } break;
-		case  1: { return (GetPool< 1>()->allocMem(size)); } break;
-		case  2: { return (GetPool< 2>()->allocMem(size)); } break;
-		case  3: { return (GetPool< 3>()->allocMem(size)); } break;
-		case  4: { return (GetPool< 4>()->allocMem(size)); } break;
-		case  5: { return (GetPool< 5>()->allocMem(size)); } break;
-		case  6: { return (GetPool< 6>()->allocMem(size)); } break;
-		case  7: { return (GetPool< 7>()->allocMem(size)); } break;
-		case  8: { return (GetPool< 8>()->allocMem(size)); } break;
-		case  9: { return (GetPool< 9>()->allocMem(size)); } break;
-		case 10: { return (GetPool<10>()->allocMem(size)); } break;
-		case 11: { return (GetPool<11>()->allocMem(size)); } break;
-		case 12: { return (GetPool<12>()->allocMem(size)); } break;
-		case 13: { return (GetPool<13>()->allocMem(size)); } break;
-		case 14: { return (GetPool<14>()->allocMem(size)); } break;
-		case 15: { return (GetPool<15>()->allocMem(size)); } break;
-		case 16: { return (GetPool<16>()->allocMem(size)); } break;
-		case 17: { return (GetPool<17>()->allocMem(size)); } break;
-		case 18: { return (GetPool<18>()->allocMem(size)); } break;
-		case 19: { return (GetPool<19>()->allocMem(size)); } break;
-		case 20: { return (GetPool<20>()->allocMem(size)); } break;
-		case 21: { return (GetPool<21>()->allocMem(size)); } break;
-		case 22: { return (GetPool<22>()->allocMem(size)); } break;
-		case 23: { return (GetPool<23>()->allocMem(size)); } break;
-		case 24: { return (GetPool<24>()->allocMem(size)); } break;
-		case 25: { return (GetPool<25>()->allocMem(size)); } break;
-		case 26: { return (GetPool<26>()->allocMem(size)); } break;
-		case 27: {                                         } break;
-		case 28: {                                         } break;
-		case 29: {                                         } break;
-		case 30: {                                         } break;
-		case 31: {                                         } break;
-		default: {                                         } break;
-	}
-
-	// allocation too large, handle externally
-	return nullptr;
-}
-
-void LuaMemPool::PoolImpl::Free(void* ptr, uint32_t size) {
-	const uint32_t subPoolIndex = CalcPoolIndex(size);
-
-	numAllocs[subPoolIndex] -= 1;
-	allocSums[subPoolIndex] -= size;
-	numAllocs[   NUM_POOLS] -= 1;
-	allocSums[   NUM_POOLS] -= size;
-
-	assert(ptr != nullptr);
-
-	switch (subPoolIndex) {
-		case  0: { return (GetPool< 0>()->freeMem(ptr)); } break;
-		case  1: { return (GetPool< 1>()->freeMem(ptr)); } break;
-		case  2: { return (GetPool< 2>()->freeMem(ptr)); } break;
-		case  3: { return (GetPool< 3>()->freeMem(ptr)); } break;
-		case  4: { return (GetPool< 4>()->freeMem(ptr)); } break;
-		case  5: { return (GetPool< 5>()->freeMem(ptr)); } break;
-		case  6: { return (GetPool< 6>()->freeMem(ptr)); } break;
-		case  7: { return (GetPool< 7>()->freeMem(ptr)); } break;
-		case  8: { return (GetPool< 8>()->freeMem(ptr)); } break;
-		case  9: { return (GetPool< 9>()->freeMem(ptr)); } break;
-		case 10: { return (GetPool<10>()->freeMem(ptr)); } break;
-		case 11: { return (GetPool<11>()->freeMem(ptr)); } break;
-		case 12: { return (GetPool<12>()->freeMem(ptr)); } break;
-		case 13: { return (GetPool<13>()->freeMem(ptr)); } break;
-		case 14: { return (GetPool<14>()->freeMem(ptr)); } break;
-		case 15: { return (GetPool<15>()->freeMem(ptr)); } break;
-		case 16: { return (GetPool<16>()->freeMem(ptr)); } break;
-		case 17: { return (GetPool<17>()->freeMem(ptr)); } break;
-		case 18: { return (GetPool<18>()->freeMem(ptr)); } break;
-		case 19: { return (GetPool<19>()->freeMem(ptr)); } break;
-		case 20: { return (GetPool<20>()->freeMem(ptr)); } break;
-		case 21: { return (GetPool<21>()->freeMem(ptr)); } break;
-		case 22: { return (GetPool<22>()->freeMem(ptr)); } break;
-		case 23: { return (GetPool<23>()->freeMem(ptr)); } break;
-		case 24: { return (GetPool<24>()->freeMem(ptr)); } break;
-		case 25: { return (GetPool<25>()->freeMem(ptr)); } break;
-		case 26: { return (GetPool<26>()->freeMem(ptr)); } break;
-		case 27: {                                       } break;
-		case 28: {                                       } break;
-		case 29: {                                       } break;
-		case 30: {                                       } break;
-		case 31: {                                       } break;
-	}
-}
-
--- rts/Lua/LuaMemPool.h
+++ rts/Lua/LuaMemPool.h
@@ -1,10 +1,10 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-#ifndef LUA_MEM_POOL_H_
-#define LUA_MEM_POOL_H_
+#pragma once
 
 #include <cstddef>
 #include <vector>
+#include <memory>
 
 #include "System/bitops.h"
 #include "System/MemPoolTypes.h"
@@ -24,7 +24,7 @@ public:
 		if (!LuaMemPool::enabled)
 			return;
 
-		poolImpl.Kill();
+		luaMemPoolImpl = nullptr;
 	}
 
 	LuaMemPool(const LuaMemPool& p) = delete;
@@ -45,141 +45,39 @@ public:
 	static void KillStatic();
 
 public:
-	void Clear() {
-		DeleteBlocks();
-		ClearStats(true);
-		ClearTables();
-	}
-
-	void Reserve(size_t size) {
-		#if (LMP_USE_CHUNK_TABLE == 1)
-		freeChunksTable.reserve(size);
-		chunkCountTable.reserve(size);
-
-		#if 1
-		allocBlocks.reserve(size / 16);
-		#endif
-		#endif
-	}
-
-	void DeleteBlocks();
+	void Clear();
 	void* Alloc(size_t size);
 	void* Realloc(void* ptr, size_t nsize, size_t osize);
 	void Free(void* ptr, size_t size);
 
-	void LogStats(const char* handle, const char* lctype) const;
-	void ClearStats(bool b) {
-		allocStats[STAT_NIA] *= (1 - b);
-		allocStats[STAT_NEA] *= (1 - b);
-		allocStats[STAT_NRA] *= (1 - b);
-		allocStats[STAT_NCB] *= (1 - b);
-		allocStats[STAT_NBB] *= (1 - b);
-
-		#if (LMP_USE_CHUNK_TABLE == 0)
-		poolImpl.numAllocs.fill(0);
-		poolImpl.allocSums.fill(0);
-		#endif
-	}
-
-	void ClearTables() {
-		#if (LMP_USE_CHUNK_TABLE == 1)
-		freeChunksTable.clear();
-		chunkCountTable.clear();
-		#endif
-	}
+	void LogStats(const char* handle, const char* lctype);
 
 	size_t  GetGlobalIndex() const { return globalIndex; }
 	size_t  GetSharedCount() const { return sharedCount; }
 	size_t& GetSharedCount()       { return sharedCount; }
 
 public:
-	static constexpr size_t MIN_ALLOC_SIZE = sizeof(void*);
-	static constexpr size_t MAX_ALLOC_SIZE = 1 << 26; // Because poolPtrs[26] = NewPool<26>(); is the last allocated pool
-	// static constexpr size_t MAX_ALLOC_SIZE = (1024 * 1024) - 1;
-
 	static bool enabled;
-
 private:
-	#if (LMP_USE_CHUNK_TABLE == 1)
-	spring::unsynced_map<size_t, void*> freeChunksTable;
-	spring::unsynced_map<size_t, size_t> chunkCountTable;
-
-	std::vector<void*> allocBlocks;
-	#endif
-
-
-	#if (LMP_USE_CHUNK_TABLE == 0)
-	struct PoolImpl {
-	public:
-		static constexpr uint32_t NUM_POOLS = 32;
-
-		// all N's intentionally over-dimensioned by a factor 1<<10
-		static constexpr std::array<uint32_t, NUM_POOLS> NUM_CHUNKS = {{
-			1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,
-			1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,
-			1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,
-			1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,
-		}};
-		static constexpr std::array<uint32_t, NUM_POOLS> NUM_PAGES = {{
-			1 << 15,  1 << 15,  1 << 14,  1 << 14,  1 << 13,  1 << 13,  1 << 12,  1 << 12,
-			1 << 11,  1 << 11,  1 << 10,  1 << 10,  1 <<  9,  1 <<  9,  1 <<  8,  1 <<  8,
-			1 <<  7,  1 <<  7,  1 <<  6,  1 <<  6,  1 <<  5,  1 <<  5,  1 <<  4,  1 <<  4,
-			1 <<  3,  1 <<  3,  1 <<  2,  1 <<  2,  1 <<  1,  1 <<  1,  1 <<  0,  1 <<  0,
-		}};
-
-		std::array<uint8_t[sizeof(FixedDynMemPool<0, NUM_CHUNKS[NUM_POOLS - 1], 0>)], NUM_POOLS> memPools;
-		std::array<void*, NUM_POOLS> poolPtrs;
-
-		std::array<size_t, NUM_POOLS + 1> numAllocs;
-		std::array<size_t, NUM_POOLS + 1> allocSums;
-
-	public:
-		static uint32_t CalcPoolIndex(uint32_t alloc) {
-			// skip first few pools due to page-overhead
-			return (std::max(2u + (MIN_ALLOC_SIZE == 8), log_base_2(alloc)));
-		}
-
-
-		template<size_t i, typename PoolType = FixedDynMemPool<1 << i, NUM_CHUNKS[i], NUM_PAGES[i]>>
-		PoolType* NewPool() {
-			static_assert(sizeof(memPools[i]) >= sizeof(PoolType), "");
-			return (new (memPools[i]) PoolType());
-		}
-
-		template<size_t i, typename PoolType = FixedDynMemPool<1 << i, NUM_CHUNKS[i], NUM_PAGES[i]>>
-		PoolType* GetPool() {
-			return (static_cast<PoolType*>(poolPtrs[i]));
-		}
-
-		template<size_t i, typename PoolType = FixedDynMemPool<1 << i, NUM_CHUNKS[i], NUM_PAGES[i]>>
-		void KillPool() {
-			GetPool<i>()->~PoolType();
-		}
-
-
-		void Init();
-		void Kill();
-
-		void* Alloc(uint32_t size);
-		void Free(void* ptr, uint32_t size);
-	};
-
-	PoolImpl poolImpl;
-	#endif
-
+	static constexpr uint32_t NUM_BUCKETS = 32;
+	static constexpr uint32_t BUCKET_STEP = 16;
+	using LuaMemPoolImpl = PassThroughPool<NUM_BUCKETS, 4 * (1024 * 1024)>;
+	std::unique_ptr<LuaMemPoolImpl> luaMemPoolImpl;
 
 	enum {
-		STAT_NIA = 0, // number of internal allocs
-		STAT_NEA = 1, // number of external allocs
-		STAT_NRA = 2, // number of recycled allocs
-		STAT_NCB = 3, // number of chunk bytes currently in use
-		STAT_NBB = 4, // number of block bytes alloced in total
+		STAT_NAI = 0, // number of internal allocs
+		STAT_NAF = 1, // number of int fail allocs
+		STAT_NAE = 2, // number of external allocs
+		STAT_NBI = 3, // number of bytes alloced (internal)
+		STAT_NBF = 4, // number of bytes alloced (int fail)
+		STAT_NBE = 5, // number of bytes alloced (external)
+		STAT_NTI = 6, // cumulative time spent on internal allocs
+		STAT_NTF = 7, // cumulative time spent on int fail allocs
+		STAT_NTE = 8, // cumulative time spent on external allocs
 	};
 
-	size_t allocStats[5] = {0, 0, 0, 0, 0};
+	std::array<uint64_t, 9> allocStats = { 0, 0, 0, 0, 0 };
+
 	size_t globalIndex = 0;
 	size_t sharedCount = 0;
 };
-
-#endif
-
