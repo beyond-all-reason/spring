0369d6e6f1700032dda2f7d8959c00d6f62ccfbd
LHoG <1476261+lhog@users.noreply.github.com>
2023-05-04 22:16:53 +0400

    Fix forgotten ++level

--- rts/Rendering/GL/myGL.cpp
+++ rts/Rendering/GL/myGL.cpp
@@ -356,7 +356,7 @@ void glSpringTexStorage2D(GLenum target, GLint levels, GLint internalFormat, GLs
 		default: /*LOG_L(L_ERROR, "[%s] Couldn't detect format type for %i", __FUNCTION__, internalFormat);*/
 			break;
 		}
-		for (int level = 0; level < levels; level)
+		for (int level = 0; level < levels; ++level)
 			glTexImage2D(target, level, internalFormat, std::max(width >> level, 1), std::max(height >> level, 1), 0, format, type, nullptr);
 
 		glTexParameteri(target, GL_TEXTURE_BASE_LEVEL,          0);
@@ -381,7 +381,7 @@ void glSpringTexStorage3D(GLenum target, GLint levels, GLint internalFormat, GLs
 		default: /*LOG_L(L_ERROR, "[%s] Couldn't detect format type for %i", __FUNCTION__, internalFormat);*/
 			break;
 		}
-		for (int level = 0; level < levels; level)
+		for (int level = 0; level < levels; ++level)
 			glTexImage3D(target, level, internalFormat, std::max(width >> level, 1), std::max(height >> level, 1), std::max(depth >> level, 1), 0, format, type, nullptr);
 
 		glTexParameteri(target, GL_TEXTURE_BASE_LEVEL,          0);
