beyond-all-reason/spring

commit ecb3ad360adc8a08b40a7fa8b52ea5bba6ac36ce

Fix missing mv.UploadVBOs() in  CWorldDrawer::InitPost(), also store model names without extension because some calls might prefer to refer a model by its name without an extension. This should fix SIGSEGV crashes introduced with https://github.com/beyond-all-reason/spring/commit/b57b40cda165234ec24d430ed58d60feed761726 https://github.com/beyond-all-reason/spring/commit/b0b6eca06102b9bfb08d95da5059b6f4949c83ba https://github.com/beyond-all-reason/spring/commit/be4e1566d878f4111fd0181dfd44db7d3c3dd775

rts/Rendering/WorldDrawer.cpp
@@ -157,20 +157,9 @@ void CWorldDrawer::InitPost() const
 		modelLoader.DrainPreloadFutures(0);
 		auto& mv = S3DModelVAO::GetInstance();
 		if (preloadMode) {
+			mv.UploadVBOs();
 			mv.SetSafeToDeleteVectors();
 			CModelsLock::SetThreadSafety(false); //all models are already preloaded
-
-			const auto& mdlVec = modelLoader.GetModelsVec();
-			for (size_t i = 0; i < mdlVec.size(); ++i) {
-				const auto& model = mdlVec[i];
-				if (model.id == -1)
-					continue;
-
-				if (model.loadStatus != S3DModel::LoadStatus::LOADED) {
-					const std::string err = fmt::format("ML Error. ModelName {}, ModelID {}, numPieces {}, LS {}", model.name, model.id, model.numPieces, static_cast<uint32_t>(model.loadStatus));
-					throw std::runtime_error(err);
-				}
-			}
 		}
 	}
 }

rts/Rendering/Models/3DModelVAO.cpp
@@ -154,6 +154,7 @@ void S3DModelVAO::UploadVBOs()
 	bool reinitVAO = (vao.GetIdRaw() == 0);
 
 	if (vertData.size() > vertUploadIndex) {
+		assert(!safeToDeleteVectors);
 		vertVBO.Bind();
 		const size_t reqSize = AlignUp(std::max(vertData.size(), S3DModelVAO::VERT_SIZE0) * sizeof(SVertexData), MEM_STEP);
 		reinitVAO |= (reqSize > vertVBO.GetSize());
@@ -165,6 +166,7 @@ void S3DModelVAO::UploadVBOs()
 	}
 
 	if (indxData.size() > indxUploadIndex) {
+		assert(!safeToDeleteVectors);
 		indxVBO.Bind();
 		const size_t reqSize = AlignUp(std::max(indxData.size(), S3DModelVAO::INDX_SIZE0) * sizeof(   uint32_t), MEM_STEP);
 		reinitVAO |= (reqSize > indxVBO.GetSize());

rts/Rendering/Models/IModelParser.cpp
@@ -292,32 +292,47 @@ S3DModel* CModelLoader::LoadModel(std::string name, bool preload)
 	return model;
 }
 
-S3DModel* CModelLoader::GetCachedModel(std::string name)
+S3DModel* CModelLoader::GetCachedModel(std::string fullName)
 {
 	// caller has mutex lock
 
 	static const auto CompPred = [](auto&& lhs, auto&& rhs) { return lhs.first < rhs.first; };
 
 	static constexpr std::string_view O3D = "objects3d/";
-	if (auto oi = name.find(O3D); oi != std::string::npos) {
+	if (auto oi = fullName.find(O3D); oi != std::string::npos) {
 		assert(oi == 0u);
-		name.erase(0, O3D.size());
+		fullName.erase(0, O3D.size());
 	}
 
-	auto key = std::make_pair(name, uint32_t(-1));
-	const auto ci = spring::BinarySearch(cache.begin(), cache.end(), key, CompPred);
+	auto keyFull = std::make_pair(fullName, uint32_t(-1));
+	const auto ci = spring::BinarySearch(cache.begin(), cache.end(), keyFull, CompPred);
 	if (ci != cache.end()) {
-		assert(name == ci->first);
+		assert(keyFull.first == ci->first);
 		return &models[ci->second];
 	}
 
+	auto keyName = std::pair<std::string, uint32_t>("", uint32_t(-1));
+	if (const auto ext = FileSystem::GetExtension(fullName); !ext.empty()) {
+		keyName.first = fullName.substr(0, fullName.size() - ext.size() - 1);
+		const auto ci = spring::BinarySearch(cache.begin(), cache.end(), keyName, CompPred);
+		if (ci != cache.end()) {
+			assert(keyName.first == ci->first);
+			return &models[ci->second];
+		}
+	}
+
 	if (modelID + 1 == MAX_MODEL_OBJECTS) {
-		LOG_L(L_ERROR, "[CModelLoader::%s] Model pool of size %i is exhausted. Cannot load model %s", __func__, MAX_MODEL_OBJECTS, name.c_str());
+		LOG_L(L_ERROR, "[CModelLoader::%s] Model pool of size %i is exhausted. Cannot load model %s", __func__, MAX_MODEL_OBJECTS, fullName.c_str());
 		return &models[0]; //dummy model
 	}
 
 	models[modelID].id = ++modelID;
-	spring::VectorInsertSorted(cache, std::make_pair(name, modelID), CompPred);
+	keyFull.second = models[modelID].id;
+	keyName.second = models[modelID].id;
+
+	spring::VectorInsertSorted(cache, std::move(keyFull), CompPred);
+	if (!keyName.first.empty())
+		spring::VectorInsertSorted(cache, std::move(keyName), CompPred);
 
 	return &models[modelID];
 }

commit 8d06b7b623b6b7cbc485bd52f758609f159490a8

Fix LoadingMT regression introduced in https://github.com/beyond-all-reason/spring/commit/ecb3ad360adc8a08b40a7fa8b52ea5bba6ac36ce

rts/Rendering/WorldDrawer.cpp
@@ -151,13 +151,16 @@ void CWorldDrawer::InitPost() const
 	{
 		ISky::GetSky()->SetupFog();
 	}
-	lock = {}; //unlock, no point in locking it further
+	lock = {}; //unlock
 	{
 		loadscreen->SetLoadMessage("Finalizing Models");
 		modelLoader.DrainPreloadFutures(0);
 		auto& mv = S3DModelVAO::GetInstance();
 		if (preloadMode) {
-			mv.UploadVBOs();
+			{
+				auto lock = CLoadLock::GetUniqueLock();
+				mv.UploadVBOs();
+			}
 			mv.SetSafeToDeleteVectors();
 			CModelsLock::SetThreadSafety(false); //all models are already preloaded
 		}