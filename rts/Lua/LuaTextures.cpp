93e627616cf1bba1599a09369d62f5dae8baca9a
Krogoth100 <totalcontinuity.spring@gmail.com>
2023-07-20 00:51:27 +0300

    small fix

--- rts/Lua/LuaTextures.cpp
+++ rts/Lua/LuaTextures.cpp
@@ -47,8 +47,8 @@ std::string LuaTextures::Create(const Texture& tex)
 
 	glClearErrors("LuaTex", __func__, globalRendering->glDebugErrors);
 
-	GLenum dataFormat = Texture::GetInternalFormatDataFormat(tex.format);
-	GLenum dataType   = Texture::GetInternalFormatDataType(tex.format);
+	GLenum dataFormat = ::Texture::GetInternalFormatDataFormat(tex.format);
+	GLenum dataType   = ::Texture::GetInternalFormatDataType(tex.format);
 
 	switch (tex.target) {
 		case GL_TEXTURE_1D: {
