affc3aa16bd49115e24ad4abcf530b5c749536bf
Marcus Hutchings <github-2843@nevermore.plus.com>
2023-08-15 08:50:32 +0100

    added important note on groups used

--- rts/Sim/Path/QTPFS/Registry.h
+++ rts/Sim/Path/QTPFS/Registry.h
@@ -1,6 +1,16 @@
 #ifndef QTPFS_REGISTRY_H__
 #define QTPFS_REGISTRY_H__
 
+/**
+ * Important! Group ownerships used on this registry:
+ * 
+ * group<PathSearch, ProcessPath>()
+ *  used to group path searches to be processed this frame
+ *  those searches are removed afterwards; however if a request was a raw path and failed, then
+ *  a new path gets scheduled for a regular path. By using groups, added new paths does not break
+ *  the loop used to cleared out processed paths.
+ */
+
 #include "System/Ecs/EcsMain.h"
 #include "System/Ecs/Utils/DoubleLinkedList.h"
 #include "System/Ecs/Utils/SystemGlobalUtils.h"
