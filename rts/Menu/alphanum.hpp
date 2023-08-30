c5ffe672731cb6df88ad9625ccae9c988d8547d1
sprunk <spr.ng@o2.pl>
2023-08-30 02:01:41 +0200

    Get rid of `{u,bi}nary_function` functor inheritance
    No longer needed, containers/algorithms use concepts nowadays.

--- rts/Menu/alphanum.hpp
+++ rts/Menu/alphanum.hpp
@@ -265,7 +265,7 @@ namespace doj {
 	implement "std::ostream operator<< (std::ostream&, const Ty&)".
 	*/
 	template<class Ty>
-	struct alphanum_less : public spring::binary_function<Ty, Ty, bool> {
+	struct alphanum_less {
 		bool operator()(const Ty& left, const Ty& right) const {
 			return alphanum_comp(left, right) < 0;
 		}
