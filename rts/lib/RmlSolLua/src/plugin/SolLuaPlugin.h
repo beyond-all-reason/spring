1b1e547efae3893ef5f3996c421680c190ddf231
loveridge <git@lvrde.com>
2023-10-02 22:28:51 -0700

    rmlui lua and sdl integration

---
+++ rts/lib/RmlSolLua/src/plugin/SolLuaPlugin.h
@@ -0,0 +1,43 @@
+﻿// RmlSolLua.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include "../RmlSolLua.h"
+
+#include <RmlUi/Lua/Header.h>
+#include <RmlUi/Core/Plugin.h>
+#include <RmlUi/Core/Platform.h>
+#include <sol2/sol.hpp>
+
+#include <memory>
+
+
+namespace Rml::SolLua
+{
+
+    class SolLuaDocumentElementInstancer;
+    class SolLuaEventListenerInstancer;
+
+    class RMLUILUA_API SolLuaPlugin : public Plugin
+    {
+    public:
+        SolLuaPlugin(sol::state_view lua_state);
+        SolLuaPlugin(sol::state_view lua_state, const Rml::String& lua_environment_identifier);
+
+    private:
+        int GetEventClasses() override;
+
+        void OnInitialise() override;
+        void OnShutdown() override;
+
+        std::unique_ptr<SolLuaDocumentElementInstancer> document_element_instancer;
+        std::unique_ptr<SolLuaEventListenerInstancer> event_listener_instancer;
+        void OnElementCreate(Element* element);
+        void OnElementDestroy(Element* element);
+
+        sol::state_view m_lua_state;
+        Rml::String m_lua_env_identifier;
+    };
+
+} // end namespace Rml::SolLua
