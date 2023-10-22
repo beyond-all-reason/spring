2afb7605af5ee2847529fdb8adac1bd6103c5c52
loveridge <git@lvrde.com>
2023-10-21 17:04:06 -0700

    fix for chobby support, better binds, translation table

---
+++ rts/Rml/RmlInputReceiver.h
@@ -0,0 +1,22 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#ifndef RMLINPUT_H
+#define RMLINPUT_H
+
+#include "Game/UI/InputReceiver.h"
+#include "Rml/Backends/RmlUi_Backend.h"
+
+class CRmlInputReceiver : public CInputReceiver
+{
+public:
+	CRmlInputReceiver() : CInputReceiver(FRONT), rml_active(false){};
+	~CRmlInputReceiver() = default;
+
+	bool IsAbove(int x, int y) { return rml_active; };
+	void setActive(bool active) { rml_active = active; };
+	bool KeyPressed(int keyCode, int scanCode, bool isRepeat) { return RmlGui::ProcessKeyPressed(keyCode, scanCode, isRepeat); }
+
+private:
+	bool rml_active;
+};
+#endif
