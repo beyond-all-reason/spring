7fafc5bf25813ad9a31be21269c417c970168964
loveridge <git@lvrde.com>
2023-11-09 21:19:01 -0700

    clean up comments

--- rts/Rml/Backends/RmlUi_Backend.h
+++ rts/Rml/Backends/RmlUi_Backend.h
@@ -39,26 +39,15 @@
 
 using KeyDownCallback = bool (*)(Rml::Context* context, Rml::Input::KeyIdentifier key, int key_modifier, float native_dp_ratio, bool priority);
 
-/**
-    This interface serves as a basic abstraction over the various backends included with RmlUi. It is mainly intended as an example to get something
-    simple up and running, and provides just enough functionality for the included samples.
-
-    This interface may be used directly for simple applications and testing. However, for anything more advanced we recommend to use the backend as a
-    starting point and copy relevant parts into the main loop of your application. On the other hand, the underlying platform and renderer used by the
-    backend are intended to be re-usable as is.
- */
 namespace RmlGui {
 
-// Initializes the backend, including the custom system and render interfaces, and opens a window for rendering the RmlUi context.
 bool Initialize(SDL_Window* target_window, SDL_GLContext target_glcontext, int winX, int winY);
 bool InitializeLua(lua_State* lua_state);
-// Closes the window and release all resources owned by the backend, including the system and render interfaces.
+
 void Shutdown();
 void Reload();
 
-// Returns a pointer to the custom system interface which should be provided to RmlUi.
 Rml::SystemInterface* GetSystemInterface();
-// Returns a pointer to the custom render interface which should be provided to RmlUi.
 Rml::RenderInterface* GetRenderInterface();
 
 bool ProcessEvent(const SDL_Event& event);
@@ -68,20 +57,16 @@ bool ProcessKeyPressed(int keyCode, int scanCode, bool isRepeat);
 bool ProcessKeyReleased(int keyCode, int scanCode);
 bool ProcessTextInput(const std::string& text);
 
-// Request application closure during the next event processing call.
-void RequestExit();
 void ToggleDebugger();
 
 void Update();
-
 void RenderFrame();
+
 void CreateContext(const std::string& name);
 void CreateOverlayContext();
 void AddContext(Rml::Context *context);
 
-// Prepares the render state to accept rendering commands from RmlUi, call before rendering the RmlUi context.
 void BeginFrame();
-// Presents the rendered frame to the screen, call after rendering the RmlUi context.
 void PresentFrame();
 
 }
