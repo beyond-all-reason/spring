b8d78b7111c9910c68015d3f794cd1f8adae4de5
Marcus Hutchings <github-2843@nevermore.plus.com>
2024-01-03 21:56:38 +0000

    Add Requeue system.

---
+++ rts/Sim/Path/QTPFS/Systems/RequeuePathsSystem.cpp
@@ -0,0 +1,42 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#include "RequeuePathsSystem.h"
+
+#include "Sim/Path/IPathManager.h"
+#include "Sim/Path/QTPFS/Components/Path.h"
+#include "Sim/Path/QTPFS/PathManager.h"
+#include "Sim/Path/QTPFS/Registry.h"
+
+#include "System/Ecs/EcsMain.h"
+#include "System/Ecs/Utils/SystemGlobalUtils.h"
+#include "System/TimeProfiler.h"
+#include "System/Log/ILog.h"
+
+using namespace SystemGlobals;
+using namespace QTPFS;
+
+
+void RequeuePathsSystem::Init()
+{
+    systemUtils.OnUpdate().connect<&RequeuePathsSystem::Update>();
+}
+
+void RequeuePathsSystem::Update()
+{
+    SCOPED_TIMER("ECS::RequeuePathsSystem::Update");
+
+    auto* pm = dynamic_cast<PathManager*>(pathManager);
+    auto view = registry.view<PathRequeueSearch>();
+    for (auto pathEntity : view) {
+        bool &requeueSearch = view.get<PathRequeueSearch>(pathEntity).value;
+        if (requeueSearch) {
+            pm->RequeueSearch(&registry.get<IPath>(pathEntity), true, false);
+            registry.emplace_or_replace<PathUpdatedCounterIncrease>(pathEntity);
+            requeueSearch = false;
+        }
+    }
+}
+
+void RequeuePathsSystem::Shutdown() {
+    systemUtils.OnUpdate().disconnect<&RequeuePathsSystem::Update>();
+}
