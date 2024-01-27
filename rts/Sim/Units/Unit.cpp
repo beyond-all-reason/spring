89285f3b672e6176ad36b7fdd13a102e64eb7bcc
LHoG <1476261+lhog@users.noreply.github.com>
2024-01-27 12:28:15 +0400

    Update height of units being built. Fixes https://github.com/beyond-all-reason/spring/issues/1241

--- rts/Sim/Units/Unit.cpp
+++ rts/Sim/Units/Unit.cpp
@@ -985,13 +985,15 @@ void CUnit::SlowUpdate()
 			if (health <= 0.0f || buildProgress <= 0.0f)
 				KillUnit(nullptr, false, true);
 		}
+		moveType->SlowUpdate();
 
 		ScriptDecloak(nullptr, nullptr);
 		return;
 	}
 
-	// below is stuff that should not be run while being built
+	// should not be run while being built
 	commandAI->SlowUpdate();
+
 	moveType->SlowUpdate();
 
 
