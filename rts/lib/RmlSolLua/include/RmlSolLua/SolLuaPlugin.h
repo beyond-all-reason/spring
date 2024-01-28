d5201cb906cf5095310a87a997e72364401db2d8
loveridge <git@lvrde.com>
2024-01-28 16:40:13 -0700

    add an explicit removeLua call

--- rts/lib/RmlSolLua/include/RmlSolLua/SolLuaPlugin.h
+++ rts/lib/RmlSolLua/include/RmlSolLua/SolLuaPlugin.h
@@ -7,6 +7,7 @@
 #include <RmlUi/Core/Plugin.h>
 #include <RmlUi/Core/Platform.h>
 #include <sol2/sol.hpp>
+#include "RmlUi/Core/ElementDocument.h"
 #include "TranslationTable.h"
 
 #include <memory>
@@ -24,6 +25,8 @@ namespace Rml::SolLua
         SolLuaPlugin(sol::state_view lua_state);
         SolLuaPlugin(sol::state_view lua_state, const Rml::String& lua_environment_identifier);
 
+        void RemoveLuaItems();
+
         TranslationTable translationTable;
     private:
         int GetEventClasses() override;
@@ -31,11 +34,19 @@ namespace Rml::SolLua
         void OnInitialise() override;
         void OnShutdown() override;
 
+        void OnContextCreate(Context* context) override;
+        void OnContextDestroy(Context* context) override;
+        void OnDocumentLoad(ElementDocument* document) override;
+        void OnDocumentUnload(ElementDocument* document) override;
+
         std::unique_ptr<SolLuaDocumentElementInstancer> document_element_instancer;
         std::unique_ptr<SolLuaEventListenerInstancer> event_listener_instancer;
 
         sol::state_view m_lua_state;
         Rml::String m_lua_env_identifier;
+
+        std::vector<Context*> luaContexts;
+        std::vector<ElementDocument*> luaDocuments;
     };
 
 } // end namespace Rml::SolLua
