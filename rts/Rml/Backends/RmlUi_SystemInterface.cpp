e0f327c65fee1fbce0c2df43ff539c26ad364332
ChrisClark13 <krizasan@gmail.com>
2024-02-03 10:26:46 -0700

    use translation table

--- rts/Rml/Backends/RmlUi_SystemInterface.cpp
+++ rts/Rml/Backends/RmlUi_SystemInterface.cpp
@@ -51,7 +51,13 @@ double RmlSystemInterface::GetElapsedTime()
 
 int RmlSystemInterface::TranslateString(Rml::String& translated, const Rml::String& input)
 {
+	if (!translationTable || !translationTable->exists(input)) {
+		translated = input;
 	return 0;
+}
+	std::string translation = translationTable->getTranslationString(input);
+	translated = translation;
+	return 1;
 }
 
 bool RmlSystemInterface::LogMessage(Rml::Log::Type type, const Rml::String& message)
