cec84a0c2013a6fa89374e5fa243f553fe30f928
ChrisClark13 <krizasan@gmail.com>
2024-03-15 22:57:03 -0600

    without the render_interface being 100% finalized, it is too early for DecoratorLuaRender to exist (made fork that still has it though)

--- rts/Rml/Backends/RmlUi_Backend.cpp
+++ rts/Rml/Backends/RmlUi_Backend.cpp
@@ -39,7 +39,6 @@
 #include "Lua/LuaUI.h"
 #include "Rendering/Textures/Bitmap.h"
 #include "Rml/Components/ElementLuaTexture.h"
-#include "Rml/Components/DecoratorLuaRender.h"
 #include "Rml/RmlInputReceiver.h"
 #include "Rml/SolLua/RmlSolLua.h"
 #include "RmlUi_Backend.h"
@@ -123,7 +122,6 @@ struct BackendData {
 	CtxMutex contextMutex;
 	Rml::UniquePtr<PassThroughPlugin> plugin;
     Rml::UniquePtr<Rml::ElementInstancerGeneric<RmlGui::ElementLuaTexture>> element_lua_texture_instancer;
-	Rml::UniquePtr<RmlGui::DecoratorLuaRenderInstancer> decorator_lua_render_instancer;
 };
 
 static Rml::UniquePtr<BackendData> data;
@@ -163,9 +161,6 @@ bool RmlGui::Initialize(SDL_Window* target_window, SDL_GLContext target_glcontex
 
 	data->element_lua_texture_instancer = Rml::MakeUnique<Rml::ElementInstancerGeneric<ElementLuaTexture>>();
 	Rml::Factory::RegisterElementInstancer("lua-texture", data->element_lua_texture_instancer.get());
-
-	data->decorator_lua_render_instancer = Rml::MakeUnique<RmlGui::DecoratorLuaRenderInstancer>();
-	Rml::Factory::RegisterDecoratorInstancer("lua-render", data->decorator_lua_render_instancer.get());
 
 	data->plugin = Rml::MakeUnique<PassThroughPlugin>(OnContextCreate, OnContextDestroy);
 	Rml::RegisterPlugin(data->plugin.get());
