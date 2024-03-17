1c9f1276ffa8517194b0422cfb6717d1e1253cfa
ChrisClark13 <krizasan@gmail.com>
2024-03-17 13:18:33 -0600

    add mouse cursor alias stuff to example rml_setup.lua

--- cont/LuaUI/rml_setup.lua
+++ cont/LuaUI/rml_setup.lua
@@ -12,3 +12,15 @@ RmlGuard = true
 
 -- Load fonts
 RmlUi.LoadFontFace("Fonts/FreeMonoBold.ttf", true)
+
+-- Mouse Cursor Aliases
+--[[
+	These let standard CSS cursor names be used when doing styling.
+	If a cursor set via RCSS does not have an alias, it is unchanged.
+	CSS cursor list: https://developer.mozilla.org/en-US/docs/Web/CSS/cursor
+	RmlUi documentation: https://mikke89.github.io/RmlUiDoc/pages/rcss/user_interface.html#cursor
+]]
+
+-- when "cursor: normal" is set via RCSS, "cursornormal" will be sent to the engine... and so on for the rest
+RmlUi.SetMouseCursorAlias("default", 'cursornormal')
+RmlUi.SetMouseCursorAlias("pointer", 'Move') -- for command cursors, use the command name
