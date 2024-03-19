166f5c0196083d38d7b9c6cc238e1fc2b8404831
LHoG <1476261+lhog@users.noreply.github.com>
2024-03-19 20:00:53 +0400

    Document the fact gl.UniformXYZ can take uniform name as a first parameter in addition to the location

--- rts/Lua/LuaShaders.cpp
+++ rts/Lua/LuaShaders.cpp
@@ -991,7 +991,7 @@ int LuaShaders::SetFeatureBufferUniforms(lua_State* L) { return SetObjectBufferU
 /*** Sets the uniform float value at the locationID for the currently active shader. Shader must be activated before setting uniforms.
  *
  * @function gl.Uniform
- * @number locationID
+ * @number locationID|string uniformName
  * @number f1
  * @number[opt] f2
  * @number[opt] f3
@@ -1031,7 +1031,7 @@ int LuaShaders::Uniform(lua_State* L)
 /*** Sets the uniform int value at the locationID for the currently active shader. Shader must be activated before setting uniforms.
  *
  * @function gl.UniformInt
- * @number locationID
+ * @number locationID|string uniformName
  * @number int1
  * @number[opt] int2
  * @number[opt] int3
@@ -1094,7 +1094,7 @@ static bool GLUniformArray(lua_State* L, UniformFunc uf, ParseArrayFunc pf)
 /*** Sets the an array of uniform values at the locationID for the currently active shader. Shader must be activated before setting uniforms. Type can be one of {1 = int, 2 = float, 3 = float matrix}.
  *
  * @function gl.UniformArray
- * @number locationID
+ * @number locationID|string uniformName
  * @number type
  * @tparam table uniforms Array up to 1024 elements
  * @treturn nil
@@ -1140,7 +1140,7 @@ int LuaShaders::UniformArray(lua_State* L)
 /*** Sets the a uniform mat4 locationID for the currently active shader. Shader must be activated before setting uniforms. Can set one one common matrix like shadow, or by passing 16 additional numbers for the matrix.
  *
  * @function gl.UniformMatrix
- * @number locationID
+ * @number locationID|string uniformName
  * @tparam ?string|number m1 "shadows" | "camera" | "caminv" | "camprj"
  * @number[opt] m2
  * @number[opt] mn
