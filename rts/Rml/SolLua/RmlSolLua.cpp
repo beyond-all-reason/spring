3c7d7ca39211aee6a94cc24aa89d228dee82bba2
ChrisFloofyKitsune <krizasan@gmail.com>
2024-04-07 00:43:03 -0600

    replace space indenting with tab indenting

--- rts/Rml/SolLua/RmlSolLua.cpp
+++ rts/Rml/SolLua/RmlSolLua.cpp
@@ -11,45 +11,45 @@
 namespace Rml::SolLua
 {
 
-    SolLuaPlugin* Initialise(sol::state_view* state)
-    {
-        SolLuaPlugin* slp;
-        if (state != nullptr)
-        {
-            slp = new SolLuaPlugin(*state);
-            ::Rml::RegisterPlugin(slp);
-            RegisterLua(state, slp);
-        }
-        return slp;
-    }
+	SolLuaPlugin* Initialise(sol::state_view* state)
+	{
+		SolLuaPlugin* slp;
+		if (state != nullptr)
+		{
+			slp = new SolLuaPlugin(*state);
+			::Rml::RegisterPlugin(slp);
+			RegisterLua(state, slp);
+		}
+		return slp;
+	}
 
-    SolLuaPlugin* Initialise(sol::state_view* state, const Rml::String& lua_environment_identifier)
-    {
-        SolLuaPlugin* slp;
-        if (state != nullptr)
-        {
-            slp = new SolLuaPlugin(*state, lua_environment_identifier);
-            ::Rml::RegisterPlugin(slp);
-            RegisterLua(state, slp);
-        }
-        return slp;
-    }
+	SolLuaPlugin* Initialise(sol::state_view* state, const Rml::String& lua_environment_identifier)
+	{
+		SolLuaPlugin* slp;
+		if (state != nullptr)
+		{
+			slp = new SolLuaPlugin(*state, lua_environment_identifier);
+			::Rml::RegisterPlugin(slp);
+			RegisterLua(state, slp);
+		}
+		return slp;
+	}
 
-    void RegisterLua(sol::state_view* state, SolLuaPlugin* slp)
-    {
+	void RegisterLua(sol::state_view* state, SolLuaPlugin* slp)
+	{
 		sol::table namespace_table = state->create_named_table("RmlUi");
 
-        bind_color(namespace_table);
-        bind_context(namespace_table, slp);
-        bind_datamodel(namespace_table);
-        bind_element(namespace_table);
-        bind_element_derived(namespace_table);
-        bind_element_form(namespace_table);
-        bind_document(namespace_table);
-        bind_event(namespace_table);
-        bind_global(namespace_table, slp);
-        bind_vector(namespace_table);
-        bind_convert(namespace_table);
-    }
+		bind_color(namespace_table);
+		bind_context(namespace_table, slp);
+		bind_datamodel(namespace_table);
+		bind_element(namespace_table);
+		bind_element_derived(namespace_table);
+		bind_element_form(namespace_table);
+		bind_document(namespace_table);
+		bind_event(namespace_table);
+		bind_global(namespace_table, slp);
+		bind_vector(namespace_table);
+		bind_convert(namespace_table);
+	}
 
 } // end namespace Rml::SolLua
--- rts/Rml/SolLua/RmlSolLua.h
+++ rts/Rml/SolLua/RmlSolLua.h
@@ -7,29 +7,29 @@
 
 namespace sol
 {
-    class state_view;
+	class state_view;
 }
 
 namespace Rml::SolLua
 {
-    /// <summary>
-    /// Initializes RmlSolLua using the supplied Lua state.
-    /// </summary>
-    /// <param name="state">The Lua state to initialize into.</param>
-    SolLuaPlugin* Initialise(sol::state_view* state);
+	/// <summary>
+	/// Initializes RmlSolLua using the supplied Lua state.
+	/// </summary>
+	/// <param name="state">The Lua state to initialize into.</param>
+	SolLuaPlugin* Initialise(sol::state_view* state);
 
-    /// <summary>
-    /// Initializes RmlSolLua using the supplied Lua state.
-    /// Sets the Lua variable specified by lua_environment_identifier to the document's id when running Lua code.
-    /// </summary>
-    /// <param name="state">The Lua state to initialize into.</param>
-    /// <param name="lua_environment_identifier">The Lua variable name that is set to the document's id.</param>
-    SolLuaPlugin* Initialise(sol::state_view* state, const Rml::String& lua_environment_identifier);
+	/// <summary>
+	/// Initializes RmlSolLua using the supplied Lua state.
+	/// Sets the Lua variable specified by lua_environment_identifier to the document's id when running Lua code.
+	/// </summary>
+	/// <param name="state">The Lua state to initialize into.</param>
+	/// <param name="lua_environment_identifier">The Lua variable name that is set to the document's id.</param>
+	SolLuaPlugin* Initialise(sol::state_view* state, const Rml::String& lua_environment_identifier);
 
-    /// <summary>
-    /// Registers RmlSolLua into the specified Lua state.
-    /// </summary>
-    /// <param name="state">The Lua state to register into.</param>
-    void RegisterLua(sol::state_view* state, SolLuaPlugin* slp);
+	/// <summary>
+	/// Registers RmlSolLua into the specified Lua state.
+	/// </summary>
+	/// <param name="state">The Lua state to register into.</param>
+	void RegisterLua(sol::state_view* state, SolLuaPlugin* slp);
 
 } // end namespace Rml::SolLua
