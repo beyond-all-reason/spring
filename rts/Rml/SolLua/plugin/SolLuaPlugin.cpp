b65b548deef69a3a9e1d1f14ed46b3674d722828
ChrisFloofyKitsune <krizasan@gmail.com>
2024-04-07 00:43:03 -0600

    replace space indenting with tab indenting

--- rts/Rml/SolLua/plugin/SolLuaPlugin.cpp
+++ rts/Rml/SolLua/plugin/SolLuaPlugin.cpp
@@ -9,57 +9,57 @@
 
 namespace Rml::SolLua
 {
-    SolLuaPlugin::SolLuaPlugin(sol::state_view lua_state)
-        : m_lua_state{lua_state}
-    {
-    }
+	SolLuaPlugin::SolLuaPlugin(sol::state_view lua_state)
+		: m_lua_state{lua_state}
+	{
+	}
 
-    SolLuaPlugin::SolLuaPlugin(sol::state_view lua_state, const Rml::String& lua_environment_identifier)
-        : m_lua_state{lua_state}, m_lua_env_identifier{lua_environment_identifier}
-    {
-    }
+	SolLuaPlugin::SolLuaPlugin(sol::state_view lua_state, const Rml::String& lua_environment_identifier)
+		: m_lua_state{lua_state}, m_lua_env_identifier{lua_environment_identifier}
+	{
+	}
 
-    int SolLuaPlugin::GetEventClasses()
-    {
-        return EVT_BASIC | EVT_DOCUMENT;
-    }
+	int SolLuaPlugin::GetEventClasses()
+	{
+		return EVT_BASIC | EVT_DOCUMENT;
+	}
 
-    void SolLuaPlugin::AddContextTracking(Context* context) {
-        luaContexts.emplace_back(context);
-    }
+	void SolLuaPlugin::AddContextTracking(Context* context) {
+		luaContexts.emplace_back(context);
+	}
 
-    void SolLuaPlugin::OnContextDestroy(Context* context) {
-        luaContexts.erase(std::remove(luaContexts.begin(), luaContexts.end(), context), luaContexts.end());
-    }
+	void SolLuaPlugin::OnContextDestroy(Context* context) {
+		luaContexts.erase(std::remove(luaContexts.begin(), luaContexts.end(), context), luaContexts.end());
+	}
 
-    void SolLuaPlugin::AddDocumentTracking(ElementDocument* document) {
-        luaDocuments.emplace_back(document);
-    }
+	void SolLuaPlugin::AddDocumentTracking(ElementDocument* document) {
+		luaDocuments.emplace_back(document);
+	}
 
-    void SolLuaPlugin::OnDocumentUnload(ElementDocument* document) {
-        luaDocuments.erase(std::remove(luaDocuments.begin(), luaDocuments.end(), document), luaDocuments.end());
-    }
+	void SolLuaPlugin::OnDocumentUnload(ElementDocument* document) {
+		luaDocuments.erase(std::remove(luaDocuments.begin(), luaDocuments.end(), document), luaDocuments.end());
+	}
 
-    void SolLuaPlugin::RemoveLuaItems(){
-        for(auto d: luaDocuments) {
-            d->Close();
-        }
-        for(auto c: luaContexts) {
-            Rml::RemoveContext(c->GetName());
-        }
-    }
+	void SolLuaPlugin::RemoveLuaItems(){
+		for(auto d: luaDocuments) {
+			d->Close();
+		}
+		for(auto c: luaContexts) {
+			Rml::RemoveContext(c->GetName());
+		}
+	}
 
-    void SolLuaPlugin::OnInitialise()
-    {
-        document_element_instancer = std::make_unique<SolLuaDocumentElementInstancer>(m_lua_state, m_lua_env_identifier);
-        event_listener_instancer = std::make_unique<SolLuaEventListenerInstancer>(m_lua_state);
-        Factory::RegisterElementInstancer("body", document_element_instancer.get());
-        Factory::RegisterEventListenerInstancer(event_listener_instancer.get());
-    }
+	void SolLuaPlugin::OnInitialise()
+	{
+		document_element_instancer = std::make_unique<SolLuaDocumentElementInstancer>(m_lua_state, m_lua_env_identifier);
+		event_listener_instancer = std::make_unique<SolLuaEventListenerInstancer>(m_lua_state);
+		Factory::RegisterElementInstancer("body", document_element_instancer.get());
+		Factory::RegisterEventListenerInstancer(event_listener_instancer.get());
+	}
 
-    void SolLuaPlugin::OnShutdown()
-    {
-        // m_lua_state.collect_garbage();
-        delete this;
-    }
+	void SolLuaPlugin::OnShutdown()
+	{
+		// m_lua_state.collect_garbage();
+		delete this;
+	}
 } // end namespace Rml::SolLua
--- rts/Rml/SolLua/plugin/SolLuaPlugin.h
+++ rts/Rml/SolLua/plugin/SolLuaPlugin.h
@@ -23,37 +23,37 @@
 namespace Rml::SolLua
 {
 
-    class SolLuaDocumentElementInstancer;
-    class SolLuaEventListenerInstancer;
+	class SolLuaDocumentElementInstancer;
+	class SolLuaEventListenerInstancer;
 
-    class RMLUILUA_API SolLuaPlugin : public Plugin
-    {
-    public:
-        SolLuaPlugin(sol::state_view lua_state);
-        SolLuaPlugin(sol::state_view lua_state, const Rml::String& lua_environment_identifier);
+	class RMLUILUA_API SolLuaPlugin : public Plugin
+	{
+	public:
+		SolLuaPlugin(sol::state_view lua_state);
+		SolLuaPlugin(sol::state_view lua_state, const Rml::String& lua_environment_identifier);
 
-        void RemoveLuaItems();
-        void AddContextTracking(Context* context);
-        void AddDocumentTracking(ElementDocument* document);
+		void RemoveLuaItems();
+		void AddContextTracking(Context* context);
+		void AddDocumentTracking(ElementDocument* document);
 
-        TranslationTable translationTable;
-    private:
-        int GetEventClasses() override;
+		TranslationTable translationTable;
+	private:
+		int GetEventClasses() override;
 
-        void OnInitialise() override;
-        void OnShutdown() override;
+		void OnInitialise() override;
+		void OnShutdown() override;
 
-        void OnContextDestroy(Context* context) override;
-        void OnDocumentUnload(ElementDocument* document) override;
+		void OnContextDestroy(Context* context) override;
+		void OnDocumentUnload(ElementDocument* document) override;
 
-        std::unique_ptr<SolLuaDocumentElementInstancer> document_element_instancer;
-        std::unique_ptr<SolLuaEventListenerInstancer> event_listener_instancer;
+		std::unique_ptr<SolLuaDocumentElementInstancer> document_element_instancer;
+		std::unique_ptr<SolLuaEventListenerInstancer> event_listener_instancer;
 
-        sol::state_view m_lua_state;
-        Rml::String m_lua_env_identifier;
+		sol::state_view m_lua_state;
+		Rml::String m_lua_env_identifier;
 
-        std::vector<Context*> luaContexts;
-        std::vector<ElementDocument*> luaDocuments;
-    };
+		std::vector<Context*> luaContexts;
+		std::vector<ElementDocument*> luaDocuments;
+	};
 
 } // end namespace Rml::SolLua
