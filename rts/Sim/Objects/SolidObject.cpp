04c81acc5e14e19c7ed332222e59cfdc872434a3
Jordan Cottle <jordancottle622@gmail.com>
2024-05-02 03:16:23 -0400

    Improve variable names to be more clear

--- rts/Sim/Objects/SolidObject.cpp
+++ rts/Sim/Objects/SolidObject.cpp
@@ -97,8 +97,9 @@ void CSolidObject::UpdatePhysicalState(float eps)
 {
 	RECOIL_DETAILED_TRACY_ZONE;
 	const float waterLevel = CGround::GetWaterLevel(pos.x, pos.z);
-	const float gh = CGround::GetHeightReal(pos.x, pos.z);
-	const float wh = std::max(gh, waterLevel);
+	const float groundHeight = CGround::GetHeightReal(pos.x, pos.z);
+	// Get height of whichever surface is higher between ground and water
+	const float topSurfaceHeight = std::max(groundHeight, waterLevel);
 
 	unsigned int ps = physicalState;
 
@@ -114,13 +115,13 @@ void CSolidObject::UpdatePhysicalState(float eps)
 	//   the height property is used for much fewer purposes
 	//   than radius, so less reliable for determining state
 	#define MASK_NOAIR (PSTATE_BIT_ONGROUND | PSTATE_BIT_INWATER | PSTATE_BIT_UNDERWATER | PSTATE_BIT_UNDERGROUND)
-	ps |= (PSTATE_BIT_ONGROUND    * ((   pos.y -         gh) <=  eps));
+	ps |= (PSTATE_BIT_ONGROUND    * ((   pos.y -         groundHeight) <=  eps));
 	ps |= (PSTATE_BIT_INWATER     * ((   pos.y             ) <= waterLevel));
 //	ps |= (PSTATE_BIT_UNDERWATER  * ((   pos.y +     height) <  0.0f));
-//	ps |= (PSTATE_BIT_UNDERGROUND * ((   pos.y +     height) <    gh));
+//	ps |= (PSTATE_BIT_UNDERGROUND * ((   pos.y +     height) <    groundHeight));
 	ps |= (PSTATE_BIT_UNDERWATER  * ((midPos.y +     radius) <  waterLevel));
-	ps |= (PSTATE_BIT_UNDERGROUND * ((midPos.y +     radius) <    gh));
-	ps |= (PSTATE_BIT_INAIR       * ((   pos.y -         wh) >   eps));
+	ps |= (PSTATE_BIT_UNDERGROUND * ((midPos.y +     radius) <    groundHeight));
+	ps |= (PSTATE_BIT_INAIR       * ((   pos.y -         topSurfaceHeight) >   eps));
 	ps |= (PSTATE_BIT_INAIR       * ((    ps   & MASK_NOAIR) ==    0));
 	#undef MASK_NOAIR
 
@@ -130,7 +131,7 @@ void CSolidObject::UpdatePhysicalState(float eps)
 	// fails then A and B *must* both be false
 	//
 	// problem case: pos.y < eps (but > 0) &&
-	// gh < -eps causes ONGROUND and INAIR to
+	// groundHeight < -eps causes ONGROUND and INAIR to
 	// both be false but INWATER will fail too
 	#if 0
 	assert((IsInAir() != IsOnGround()) || IsInWater());
