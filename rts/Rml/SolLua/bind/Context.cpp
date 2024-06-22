d1ecbdb29674d34692bdd3ad85ecafd94e1c25a0
ChrisFloofyKitsune <krizasan@gmail.com>
2024-06-22 10:02:21 -0600

    remove outdated commented-out code

--- rts/Rml/SolLua/bind/Context.cpp
+++ rts/Rml/SolLua/bind/Context.cpp
@@ -251,14 +251,10 @@ void bind_context(sol::table& namespace_table, SolLuaPlugin* slp)
 		"GetElementAtPoint", sol::overload(&element::getElementAtPoint1, &element::getElementAtPoint2),
 		"PullDocumentToFront", &Rml::Context::PullDocumentToFront,
 		"PushDocumentToBack",&Rml::Context::PushDocumentToBack, "UnfocusDocument", &Rml::Context::UnfocusDocument,
-		// RemoveEventListener
 
 		// G+S
 		"dimensions", sol::property(&Rml::Context::GetDimensions, &Rml::Context::SetDimensions),
 		"dp_ratio", sol::property(&Rml::Context::GetDensityIndependentPixelRatio, &Rml::Context::SetDensityIndependentPixelRatio),
-		//--
-//		"clip_region", sol::property(&Rml::Context::GetActiveClipRegion, &Rml::Context::SetActiveClipRegion),
-
 		// G
 		"documents", sol::readonly_property(&getIndexedTable<SolLuaDocument, Rml::Context, &document::getDocument, &Rml::Context::GetNumDocuments>),
 		"focus_element", sol::readonly_property(&Rml::Context::GetFocusElement),
