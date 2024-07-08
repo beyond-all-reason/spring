0094744afa0838a0724386cb6426b6cf05a086a9
12345swordy <alexanderheistermann@gmail.com>
2024-07-08 17:48:22 -0500

    Added `Spring.GetUnitCosts` (#1586)
    First part of #1584.

--- rts/Lua/LuaSyncedRead.cpp
+++ rts/Lua/LuaSyncedRead.cpp
@@ -206,6 +206,7 @@ bool LuaSyncedRead::PushEntries(lua_State* L)
 	REGISTER_LUA_CFUNC(GetUnitIsStunned);
 	REGISTER_LUA_CFUNC(GetUnitIsBeingBuilt);
 	REGISTER_LUA_CFUNC(GetUnitResources);
+	REGISTER_LUA_CFUNC(GetUnitCosts);
 	REGISTER_LUA_CFUNC(GetUnitMetalExtraction);
 	REGISTER_LUA_CFUNC(GetUnitMaxRange);
 	REGISTER_LUA_CFUNC(GetUnitExperience);
@@ -4129,6 +4130,25 @@ int LuaSyncedRead::GetUnitResources(lua_State* L)
 	lua_pushnumber(L, unit->resourcesMake.energy);
 	lua_pushnumber(L, unit->resourcesUse.energy);
 	return 4;
+}
+
+/***
+ * @function Spring.GetUnitCosts
+ * @number unitID
+ * @treturn nil|number buildTime
+ * @treturn number metalCost
+ * @treturn number energyCost
+ */
+int LuaSyncedRead::GetUnitCosts(lua_State* L)
+{
+	const CUnit* const unit = ParseInLosUnit(L, __func__, 1);
+	if (unit == nullptr)
+		return 0;
+
+	lua_pushnumber(L, unit->buildTime);
+	lua_pushnumber(L, unit->cost.metal);
+	lua_pushnumber(L, unit->cost.energy);
+	return 3;
 }
 
 
