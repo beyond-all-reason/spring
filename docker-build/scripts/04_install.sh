7fbd84cd564a2a36fa0eef26d60f276546f6b611
Marek Rusinowski <marekrusinowski@gmail.com>
2024-09-21 12:55:56 +0200

    Drop library directories from install
    The libraries are added when we include deps via add_subdirectory.
    There isn't a simple way to ask CMake to not install them. A clean
    way would be to use components, but our cmake is not there at the
    moment.

--- docker-build/scripts/04_install.sh
+++ docker-build/scripts/04_install.sh
@@ -1,3 +1,10 @@
 cd "${BUILD_DIR}"
 
 make install
+
+# Manually remove the lib, include and share directories coming
+# from the GNUInstallDirs. Unfortunately it's not possible to
+# easily disable in CMake all installation targets exported when
+# using add_subdirectory so we drop them from installation manually.
+cd "${INSTALL_DIR}"
+rm -rf lib include share
