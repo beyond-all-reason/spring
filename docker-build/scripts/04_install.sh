2a78b224972a9519b9007616027622fb1b938d10
Marek Rusinowski <marekrusinowski@gmail.com>
2024-09-21 13:08:15 +0200

    Add why
    Co-authored-by: sprunk <spr.ng@o2.pl>

--- docker-build/scripts/04_install.sh
+++ docker-build/scripts/04_install.sh
@@ -6,5 +6,6 @@ make install
 # from the GNUInstallDirs. Unfortunately it's not possible to
 # easily disable in CMake all installation targets exported when
 # using add_subdirectory so we drop them from installation manually.
+# This is done to heavily reduce the resulting install size.
 cd "${INSTALL_DIR}"
 rm -rf lib include share
