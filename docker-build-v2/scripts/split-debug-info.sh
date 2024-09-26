b27807053413a9d10ff7f9fd5dabcfc656c6fd05
Marek Rusinowski <marekrusinowski@gmail.com>
2024-09-27 00:22:20 +0200

    entirely refactor new build pipeline

---
+++ docker-build-v2/scripts/split-debug-info.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+cd /build/out/install
+
+function split_debug {
+    set -e -u -o pipefail
+
+    file="$1"
+    if ! objdump -h "$file" | grep -q ".debug_" || objdump -h "$file" | grep -q .gnu_debuglink; then
+        echo "skipping $file"
+        return
+    fi
+    echo "stripping $file"
+    filename="$(basename "$file")"
+    debugfile="$(dirname "$file")/${filename%.*}.dbg"
+    objcopy --only-keep-debug ${file} ${debugfile}
+    strip --strip-debug --strip-unneeded ${file}
+    objcopy --add-gnu-debuglink=${debugfile} ${file}
+}
+export -f split_debug
+
+find \( -regex '.*\.\(dll\|so\|exe\)' -o -type f ! -name '*.dbg' -executable \) -print0 \
+    | xargs -0 -P1 -n1 bash -c 'split_debug "$0"'
