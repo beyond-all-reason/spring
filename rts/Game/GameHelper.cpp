70b421f0b7c425741a1e8075d368251b560ba7ab
lostsquirrel <lostsquirrel43@gmail.com>
2024-09-27 14:34:42 +0100

    Remove unnecessary logic for unit radius in bugger off rectangle.

--- rts/Game/GameHelper.cpp
+++ rts/Game/GameHelper.cpp
@@ -957,12 +957,7 @@ void CGameHelper::BuggerOffRectangle(const float3& mins, const float3& maxs, boo
 			continue;
 
 		const float footPrintRadius = math::sqrtf(footPrintX*footPrintX + footPrintZ*footPrintZ);
-
-		// If the unit radius is smaller than the moveDef foot print then the bugger off may not be honoured by the
-		// unit because the bugger off command uses unit radius to determine whether the unit has moved far enough.
-		const float addForUnitRadius = (footPrintRadius > u->radius) ? (footPrintRadius - u->radius) : 0.f;
-
-		const float buggerOffRadius = baseBuggerOffRadius + footPrintRadius + addForUnitRadius;
+		const float buggerOffRadius = baseBuggerOffRadius + footPrintRadius;
 
 		u->commandAI->BuggerOff(centreOfBuggerOff, buggerOffRadius);
 	}
