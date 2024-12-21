98427ca267ff82018e42b8092d06bfd9f291c090
ChrisFloofyKitsune <krizasan@gmail.com>
2024-12-21 14:46:34 -0700

    remove unnecessary contains check

--- rts/Rml/Backends/RmlUi_Backend.cpp
+++ rts/Rml/Backends/RmlUi_Backend.cpp
@@ -317,7 +317,7 @@ Rml::Context* RmlGui::GetOrCreateContext(const std::string& name)
 	Rml::Context* context = Rml::GetContext(name);
 	if (context == nullptr) {
 		context = Rml::CreateContext(name, {0, 0});
-	} else if (state->contexts_to_remove.contains(context)) {
+	} else {
 		// can happen if name reused on the same frame
 		state->contexts_to_remove.erase(context);
 	}
