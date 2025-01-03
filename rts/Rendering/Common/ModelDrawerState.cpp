6e5ffd6f577af99dcc85b904645ff289a424b5b8
LHoG <1476261+lhog@users.noreply.github.com>
2025-01-03 23:05:49 +0100

    Reapply "WIP"
    This reverts commit c52a1c95c8161168ff7c66ad4cfe2998a66a70b9.

--- rts/Rendering/Common/ModelDrawerState.cpp
+++ rts/Rendering/Common/ModelDrawerState.cpp
@@ -151,7 +151,7 @@ CModelDrawerStateGLSL::CModelDrawerStateGLSL()
 		modelShaders[n]->SetUniform3v("sunDiffuse", &sunLighting->modelDiffuseColor[0]);
 		modelShaders[n]->SetUniform3v("sunSpecular", &sunLighting->modelSpecularColor[0]);
 		modelShaders[n]->SetUniform("shadowDensity", sunLighting->modelShadowDensity);
-		modelShaders[n]->SetUniformMatrix4x4("shadowMatrix", false, shadowHandler.GetShadowMatrixRaw());
+		modelShaders[n]->SetUniformMatrix4x4("shadowMatrix", false, shadowHandler.GetShadowViewProjMatrix().m);
 
 		modelShaders[n]->Disable();
 		modelShaders[n]->Validate();
@@ -206,7 +206,7 @@ void CModelDrawerStateGLSL::Enable(bool deferredPass, bool alphaPass) const
 	modelShader->SetUniform3v("sunDiffuse", &sunLighting->modelDiffuseColor[0]);
 	modelShader->SetUniform3v("sunSpecular", &sunLighting->modelSpecularColor[0]);
 	modelShader->SetUniform("shadowDensity", sunLighting->modelShadowDensity);
-	modelShader->SetUniformMatrix4x4("shadowMatrix", false, shadowHandler.GetShadowMatrixRaw());
+	modelShader->SetUniformMatrix4x4("shadowMatrix", false, shadowHandler.GetShadowViewProjMatrix().m);
 
 	CModelDrawerConcept::GetLightHandler()->Update(modelShader);
 }
