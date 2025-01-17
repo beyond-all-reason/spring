dc5bf9c03ec31a0b9e3ba926e1c3b6573c4e8d54
LHoG <1476261+lhog@users.noreply.github.com>
2025-01-18 00:48:21 +0100

    Enable command line argument to produce an archive checksum to the headless/normal engines

--- rts/System/FileSystem/FileSystemInitializer.cpp
+++ rts/System/FileSystem/FileSystemInitializer.cpp
@@ -63,6 +63,16 @@ bool FileSystemInitializer::Initialize()
 
 	SetupThreadReg();
 
+	InitializeTry();
+
+	// in case of an exception, ErrorMessageBox takes care of this
+	ClearThreadReg();
+
+	return (initSuccess && !initFailure);
+}
+
+void FileSystemInitializer::InitializeTry()
+{
 	try {
 		Platform::SetOrigCWD();
 
@@ -73,7 +83,8 @@ bool FileSystemInitializer::Initialize()
 		CVFSHandler::SetGlobalInstance(new CVFSHandler("SpringVFS"));
 
 		initSuccess = true;
-	} catch (const std::exception& ex) {
+	}
+	catch (const std::exception& ex) {
 		// abort VFS-init thread
 		initFailure = true;
 
@@ -81,16 +92,12 @@ bool FileSystemInitializer::Initialize()
 		// since it can already have early observers registered that
 		// do not remove themselves until exit
 		ErrorMessageBox(ex.what(), "Spring: caught std::exception", MBF_OK | MBF_EXCL);
-	} catch (...) {
+	}
+	catch (...) {
 		initFailure = true;
 
 		ErrorMessageBox("", "Spring: caught generic exception", MBF_OK | MBF_EXCL);
 	}
-
-	// in case of an exception, ErrorMessageBox takes care of this
-	ClearThreadReg();
-
-	return (initSuccess && !initFailure);
 }
 
 void FileSystemInitializer::Cleanup(bool deallocConfigHandler)
