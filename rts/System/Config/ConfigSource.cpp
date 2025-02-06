867060b5d2dd227bea7adcc4b0b9e182bfdd7618
LHoG <1476261+lhog@users.noreply.github.com>
2025-02-06 01:00:22 +0100

    Underp

--- rts/System/Config/ConfigSource.cpp
+++ rts/System/Config/ConfigSource.cpp
@@ -128,7 +128,7 @@ char* FileConfigSource::Strip(char* begin, char* end) {
 }
 
 /**
- * @brief Sanitize file and re-read it.
+ * @brief Rewind file and re-read it.
  */
 void FileConfigSource::Read(FILE* file)
 {
