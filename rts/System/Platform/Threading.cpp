6dea8dda2c219e9f4116ed41c9d6d52abbd8f38b
lostsquirrel <lostsquirrel43@gmail.com>
2025-02-12 09:23:22 +0000

    Fix cross-compile for windows target issue with github builds.

--- rts/System/Platform/Threading.cpp
+++ rts/System/Platform/Threading.cpp
@@ -108,6 +108,63 @@ namespace Threading {
 #pragma GCC push_options
 #pragma GCC optimize ("O0")
 
+	// Mingw v12 is the minimum version that gives us a GetLogicalProcessorInformationEx() with efficiency core
+	// detection. Unfortunately mingw v12 also introduces a ticking time bomb change that can cause the toolchain to
+	// produce a crashing exe because of a change in static initialized variable space.
+	// See bug report for more information: https://sourceforge.net/p/mingw-w64/bugs/992/
+	// The net result is that we have to use GetProcAddress() on the kernel32 ourselves and provide a modified set
+	// of data definitions with the information we need.
+	// We can do away with this if 1) mingw fix their bug or 2) the build system shifts over to using MSVC.
+	namespace spring_overrides {
+
+		typedef struct _GROUP_AFFINITY {
+			KAFFINITY Mask;
+			WORD      Group;
+			WORD      Reserved[3];
+		  } GROUP_AFFINITY, *PGROUP_AFFINITY;
+
+		typedef enum _LOGICAL_PROCESSOR_RELATIONSHIP {
+			RelationProcessorCore,
+			RelationNumaNode,
+			RelationCache,
+			RelationProcessorPackage,
+			RelationGroup,
+			RelationProcessorDie,
+			RelationNumaNodeEx,
+			RelationProcessorModule,
+			RelationAll = 0xffff
+		  } LOGICAL_PROCESSOR_RELATIONSHIP;
+
+		typedef struct _PROCESSOR_RELATIONSHIP {
+			BYTE Flags;
+			BYTE EfficiencyClass;
+			BYTE Reserved[20];
+			WORD GroupCount;
+			GROUP_AFFINITY GroupMask[ANYSIZE_ARRAY];
+		  } PROCESSOR_RELATIONSHIP,*PPROCESSOR_RELATIONSHIP;
+
+		struct _SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX {
+			LOGICAL_PROCESSOR_RELATIONSHIP Relationship;
+			DWORD Size;
+			__C89_NAMELESS union {
+			  PROCESSOR_RELATIONSHIP Processor;
+			  //NUMA_NODE_RELATIONSHIP NumaNode;
+			  //CACHE_RELATIONSHIP Cache;
+			  //GROUP_RELATIONSHIP Group;
+			};
+		  };
+
+		  typedef struct _SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX, *PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX;
+	  
+		#if _WIN32_WINNT >= 0x0601
+		typedef BOOL (WINAPI *GetLogicalProcessorInformationExFunc)(
+			LOGICAL_PROCESSOR_RELATIONSHIP,
+			PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX,
+			PDWORD);
+		//WINBASEAPI WINBOOL WINAPI GetLogicalProcessorInformationEx (LOGICAL_PROCESSOR_RELATIONSHIP RelationshipType, PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX Buffer, PDWORD ReturnedLength);
+		#endif
+	}
+
 	struct ProcessorMasks {
 		uint32_t performanceCoreMask = 0;
 		uint32_t efficiencyCoreMask = 0;
@@ -119,16 +176,26 @@ namespace Threading {
 	// noop
 	#elif defined(_WIN32)
 	ProcessorMasks GetProcessorMasksWindows() {
-		PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX buffer = NULL;
-		PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX ptr = NULL;
+		spring_overrides::GetLogicalProcessorInformationExFunc Local_GetLogicalProcessorInformationEx;
+		spring_overrides::PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX buffer = NULL;
+		spring_overrides::PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX ptr = NULL;
 		DWORD returnLength = 0;
 		DWORD byteOffset = 0;
 		BOOL done = FALSE;
 		ProcessorMasks processorMasks;
 
+		Local_GetLogicalProcessorInformationEx = (spring_overrides::GetLogicalProcessorInformationExFunc) GetProcAddress(
+			GetModuleHandle(TEXT("kernel32")),
+			"GetLogicalProcessorInformationEx");
+		if (NULL == Local_GetLogicalProcessorInformationEx)
+		{
+			LOG("GetLogicalProcessorInformation is not supported.\n");
+			return processorMasks;
+		}
+
 		while (!done)
 		{
-			DWORD rc = GetLogicalProcessorInformationEx(RelationProcessorCore, buffer, &returnLength);
+			DWORD rc = Local_GetLogicalProcessorInformationEx(spring_overrides::RelationProcessorCore, buffer, &returnLength);
 
 			if (FALSE == rc)
 			{
@@ -137,7 +204,7 @@ namespace Threading {
 					if (buffer)
 						free(buffer);
 
-					buffer = (PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX)malloc(returnLength);
+					buffer = (spring_overrides::PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX)malloc(returnLength);
 
 					if (NULL == buffer)
 						return processorMasks;
@@ -152,7 +219,7 @@ namespace Threading {
 
 		while (byteOffset < returnLength)
 		{
-			if (ptr->Relationship == RelationProcessorCore)
+			if (ptr->Relationship == spring_overrides::RelationProcessorCore)
 			{
 				const uint32_t supportedMask = static_cast<uint32_t>(ptr->Processor.GroupMask[0].Mask);
 				if (supportedMask == 0) {
@@ -172,11 +239,9 @@ namespace Threading {
 				} else {
 					processorMasks.performanceCoreMask |= supportedMask;
 				}
-
-				break;
 			}
 			byteOffset += ptr->Size;
-			ptr = (PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX)(((char*)buffer) + byteOffset);
+			ptr = (spring_overrides::PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX)(((char*)buffer) + byteOffset);
 		}
 
 		if (buffer)
