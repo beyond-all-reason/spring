8f80be3f282465c3ec59cae5625662a80393c8ed
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-03-01 04:08:00 +1100

    Add widget and gadget class definitions

---
+++ rts/Lua/library/Gadget.lua
@@ -0,0 +1,27 @@
+---@meta
+
+---@class Gadget : Callins, UnsyncedCallins, SyncedCallins
+---
+---Gadgets can control game logic and receive synced and unsynced callins.
+---
+---**Attention:** Callins from `SyncedCallins` will only work on the unsynced
+---portion of the gadget.
+---
+---**Attention:** To prevent complaints from Lua Language Server, e.g.
+---
+---> `Duplicate field `CommandNotify` (duplicate-set-field)`
+---
+---Add this line at the top of your gadget script:
+---
+---```lua
+---local gadget = gadget ---@type Gadget
+---```
+---
+---@see Callins
+---@see SyncedCallins
+---@see UnsyncedCallins
+---@see Spring.IsSyncedCode
+ 
+---@type Gadget
+---@diagnostic disable-next-line: lowercase-global
+gadget = nil
