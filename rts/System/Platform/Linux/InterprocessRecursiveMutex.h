d5383705c338ac0047d94b4e5200163e9347b3b3
LHoG <1476261+lhog@users.noreply.github.com>
2025-03-21 18:32:23 +0100

    Add Linux version of InterprocessRecursiveMutex for debugging of possible build errors

---
+++ rts/System/Platform/Linux/InterprocessRecursiveMutex.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// Linux version
+
+#include <string>
+#include <cstdint>
+#include <pthread.h>
+
+class InterprocessRecursiveMutex
+{
+public:
+	using native_handle_type = pthread_mutex_t*;
+
+	InterprocessRecursiveMutex(const char* name)  noexcept(false);
+	~InterprocessRecursiveMutex() noexcept(false);
+
+	InterprocessRecursiveMutex(const InterprocessRecursiveMutex&) = delete;
+	InterprocessRecursiveMutex& operator=(const InterprocessRecursiveMutex&) = delete;
+
+	void lock();
+	bool try_lock() noexcept;
+	void unlock();
+
+	native_handle_type native_handle() { return &shData->mtx; }
+private:
+	bool TryLockImpl(uint32_t timeoutMs) noexcept;
+protected:
+	struct shared_data {
+		pthread_mutex_t mtx;
+	};
+	const std::string name;
+	int shmFd;
+	shared_data* shData;
+};
