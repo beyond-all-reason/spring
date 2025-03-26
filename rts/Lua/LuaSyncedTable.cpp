bc3721dd53a034d254c5cfb6393d633d7100cdaf
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-03-26 17:13:07 +1100

    Improve SYNCED docs

--- rts/Lua/LuaSyncedTable.cpp
+++ rts/Lua/LuaSyncedTable.cpp
@@ -71,7 +71,23 @@ static int SyncTableMetatable(lua_State* L)
 /******************************************************************************/
 
 /***
- * @table SYNCED
+ * Proxy table for reading synced global state in unsynced code.
+ * 
+ * **Generally not recommended.** Instead, listen to the same events as synced
+ * and build the table in parallel
+ * 
+ * Unsynced code can read from the synced global table (`_G`) using the `SYNCED`
+ * proxy table. e.g. `_G.foo` can be access from unsynced vai `SYNCED.foo`.
+ * 
+ * This table makes *a copy* of the object on the other side, and only copies
+ * numbers, strings, bools and tables (recursively but with the type
+ * restriction), in particular this does not allow access to functions.
+ * 
+ * Note that this makes a copy on each access, so is very slow and will not
+ * reflect changes. Cache it, but remember to refresh.
+ * 
+ * 
+ * @global SYNCED table<string, any>
  */
 bool LuaSyncedTable::PushEntries(lua_State* L)
 {
