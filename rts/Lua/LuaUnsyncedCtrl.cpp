e3171af2b664c93dfb392e0fa8fdbde3f85ffcad
Rhys van der Waerden <rhys.vdw@gmail.com>
2025-04-16 03:10:14 +1000

    Fix missing return from LuaUnsyncedCtrl::GiveOrder

--- rts/Lua/LuaUnsyncedCtrl.cpp
+++ rts/Lua/LuaUnsyncedCtrl.cpp
@@ -3354,8 +3354,10 @@ static bool CanGiveOrders(const lua_State* L)
  */
 int LuaUnsyncedCtrl::GiveOrder(lua_State* L)
 {
-	if (!CanGiveOrders(L))
+	if (!CanGiveOrders(L)) {
+		lua_pushboolean(L, false);
 		return 1;
+	}
 
 	selectedUnitsHandler.GiveCommand(LuaUtils::ParseCommand(L, __func__, 1));
 
