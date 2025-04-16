136f61b23dac3d54f4cb01398ca0370b9e47a8ea
Saurtron <saurtron@riseup.net>
2025-04-17 01:51:37 +0200

    Add some spaces and remove printf.

--- rts/System/Sync/SyncChecker.cpp
+++ rts/System/Sync/SyncChecker.cpp
@@ -13,18 +13,17 @@ unsigned CSyncChecker::g_checksum;
 int CSyncChecker::inSyncedCode;
 
 #ifdef SYNC_HISTORY
+
 unsigned CSyncChecker::nextHistoryIndex = 0;
 unsigned CSyncChecker::logs[MAX_SYNC_HISTORY];
 
 void CSyncChecker::LogHistory()
 {
 	logs[nextHistoryIndex++] = g_checksum;
-	if (nextHistoryIndex == MAX_SYNC_HISTORY) {
+	if (nextHistoryIndex == MAX_SYNC_HISTORY)
 		nextHistoryIndex = 0;
-		//LOG("[Sync::Checker] HISTORY chksum=%u\n", g_checksum);
-		printf("[Sync::Checker] HISTORY\n");
-	}
 }
+
 #endif // SYNC_HISTORY
 
 void CSyncChecker::debugSyncCheckThreading()
