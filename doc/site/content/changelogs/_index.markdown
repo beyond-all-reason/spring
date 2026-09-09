eun-ice/RecoilEngine

Highlight queued builds cancelled by placement

doc/site/content/changelogs/_index.markdown
@@ -10,4 +10,5 @@ This is the bleeding-edge changelog since version 2026.07, for **pre-release 202
 ### Build commands
 
 - queued build cancellation and rejection now apply yardmaps after their existing rectangular checks, allowing the same compatible overlaps as sequential construction. Set `construction.useYardmapsForQueuedBuildOverlap = false` in `gamedata/modrules.lua` to restore the previous rectangle-only queue behavior.
+- queued build outlines that would be removed by the previewed placement use the configurable `buildBoxOverlap` command color, which defaults to red.
 - add `Spring.TestBuildOrderOverlap(queuedBuild, proposedBuild) → boolean overlaps, boolean cancels` to test a queued build against a proposed build. `overlaps` reports any conflict under the queued-build yardmap rules; `cancels` reports that the conflict is also inside the cancellation rectangle. Each build is `{unitDefID, x, y, z, facing}`.

rts/Game/UI/CommandColors.cpp
@@ -79,6 +79,7 @@ CCommandColors::CCommandColors()
 	SETUP_COLOR(rangeInterceptorOff, 0.0f, 0.0f, 0.0f, 0.7f);
 	SETUP_COLOR(unitBox,             0.0f, 1.0f, 0.0f, 0.9f);
 	SETUP_COLOR(buildBox,            0.0f, 1.0f, 0.0f, 0.9f);
+	SETUP_COLOR(buildBoxOverlap,     1.0f, 0.0f, 0.0f, 0.9f);
 	SETUP_COLOR(allyBuildBox,        0.8f, 0.8f, 0.2f, 0.9f);
 	SETUP_COLOR(mouseBox,            1.0f, 1.0f, 1.0f, 0.9f);
 }
@@ -332,4 +333,3 @@ bool CCommandColors::LoadConfigFromString(const std::string& cfg)
 
 	return true;
 }
-

rts/Game/UI/GuiHandler.cpp
@@ -23,6 +23,7 @@
 #include "Map/MapInfo.h"
 #include "Map/MetalMap.h"
 #include "Map/ReadMap.h"
+#include "Rendering/CommandDrawer.h"
 #include "Rendering/Fonts/glFont.h"
 #include "Rendering/IconHandler.h"
 #include "Rendering/Units/UnitDrawer.h"
@@ -32,8 +33,10 @@
 #include "Rendering/Textures/NamedTextures.h"
 #include "Sim/Features/Feature.h"
 #include "Sim/Misc/LosHandler.h"
+#include "Sim/Misc/TeamHandler.h"
 #include "Sim/Units/CommandAI/CommandAI.h"
 #include "Sim/Units/CommandAI/BuilderCAI.h"
+#include "Sim/Units/QueuedBuildOverlap.h"
 #include "Sim/Units/UnitDefHandler.h"
 #include "Sim/Units/Unit.h"
 #include "Sim/Units/UnitHandler.h"
@@ -2464,22 +2467,68 @@ Command CGuiHandler::GetCommand(int mouseX, int mouseY, int buttonHint, bool pre
 	return Command(CMD_STOP);
 }
 
-
-
-static bool WouldCancelAnyQueued(const BuildInfo& b)
+static bool WouldCancelAnyQueued(const BuildInfo& buildInfo)
 {
 	RECOIL_DETAILED_TRACY_ZONE;
-	const Command c = b.CreateCommand();
+	const Command command = buildInfo.CreateCommand();
 
 	for (const int unitID: selectedUnitsHandler.selectedUnits) {
-		const CUnit* u = unitHandler.GetUnit(unitID);
+		const CUnit* unit = unitHandler.GetUnit(unitID);
 
-		if (u->commandAI->WillCancelQueued(c))
+		if (unit->commandAI->WillCancelQueued(command))
 			return true;
-
 	}
 
 	return false;
+}
+
+using CancelledBuildCommandTags = spring::unordered_map<int, spring::unordered_set<unsigned int>>;
+
+static void DrawQueuedBuildSquares(const CancelledBuildCommandTags& cancelledCommandTags)
+{
+	if (cmdColors.buildBox[3] <= 0.0f || selectedUnitsHandler.selectedUnits.empty())
+		return;
+
+	const bool drawOnShift = cmdColors.BuildBoxesOnShift() && KeyInput::GetKeyModState(KMOD_SHIFT);
+	const bool activeBuildCommand =
+		guihandler->inCommand >= 0 &&
+		guihandler->inCommand < int(guihandler->commands.size()) &&
+		guihandler->commands[guihandler->inCommand].id < 0;
+
+	if (!drawOnShift && !activeBuildCommand)
+		return;
+
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(cmdColors.UnitBoxLineWidth());
+
+	static const spring::unordered_set<unsigned int> noCancelledCommands;
+	for (const auto& [builderID, builderCAI]: unitHandler.GetBuilderCAIs()) {
+		const CUnit* builder = builderCAI->owner;
+		const float* buildColor = nullptr;
+
+		if (builder->team == gu->myTeam) {
+			buildColor = cmdColors.buildBox;
+		} else if (teamHandler.AlliedTeams(builder->team, gu->myTeam)) {
+			buildColor = cmdColors.allyBuildBox;
+		} else {
+			continue;
+		}
+
+		const auto cancelledIt = cancelledCommandTags.find(builderID);
+		const auto& cancelled = (cancelledIt != cancelledCommandTags.end()) ? cancelledIt->second : noCancelledCommands;
+		glColor4fv(buildColor);
+		commandDrawer->DrawQuedBuildingSquares(builderCAI, cancelled, cmdColors.buildBoxOverlap);
+	}
+
+	glLineWidth(1.0f);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glDisable(GL_BLEND);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
 }
 
 static void FillRowOfBuildPos(const BuildInfo& startInfo, float x, float z, float xstep, float zstep, int n, int facing, bool nocancel, std::vector<BuildInfo>& ret)
@@ -3543,6 +3592,8 @@ static inline void DrawWeaponArc(const CUnit* unit)
 void CGuiHandler::DrawMapStuff(bool onMiniMap)
 {
 	RECOIL_DETAILED_TRACY_ZONE;
+	CancelledBuildCommandTags cancelledBuildCommandTags;
+
 	if (!onMiniMap) {
 		glEnable(GL_DEPTH_TEST);
 		glDepthMask(GL_FALSE);
@@ -3835,7 +3886,6 @@ void CGuiHandler::DrawMapStuff(bool onMiniMap)
 
 				for (const BuildInfo& bi: buildInfos) {
 					const float3& buildPos = bi.pos;
-
 					DrawUnitDefRanges(nullptr, buildeeDef, buildPos);
 
 					// draw (primary) weapon range
@@ -3864,9 +3914,21 @@ void CGuiHandler::DrawMapStuff(bool onMiniMap)
 						for (const int unitID: selectedUnitsHandler.selectedUnits) {
 							const CUnit* su = unitHandler.GetUnit(unitID);
 							const CCommandAI* cai = su->commandAI;
+							const CBuilderCAI* builderCAI = dynamic_cast<const CBuilderCAI*>(cai);
+							const std::vector<Command> overlapCommands = cai->GetOverlapQueued(c);
 
-							for (const Command& cmd: cai->GetOverlapQueued(c)) {
+							for (const Command& cmd: overlapCommands) {
 								buildCommands.push_back(cmd);
+
+								const bool activeBuild =
+									builderCAI != nullptr && builderCAI->HasCurrentBuild() &&
+									!cai->commandQue.empty() && cmd.GetTag() == cai->commandQue.front().GetTag();
+								if (activeBuild)
+									continue;
+
+								const BuildInfo queuedBuild(cmd);
+								if (QueuedBuildOverlap::IsInsideCancellationRectangle(queuedBuild, bi))
+									cancelledBuildCommandTags[unitID].insert(cmd.GetTag());
 							}
 						}
 					}
@@ -3933,6 +3995,8 @@ void CGuiHandler::DrawMapStuff(bool onMiniMap)
 			}
 		}
 	}
+
+	DrawQueuedBuildSquares(cancelledBuildCommandTags);
 
 	glLineWidth(1.0f);
 
@@ -4434,4 +4498,3 @@ void CGuiHandler::DrawSelectCircle(const float3& pos, float radius,
 
 	glEnable(GL_FOG);
 }
-

beyond-all-reason/spring

doc/site/content/changelogs/_index.markdown
@@ -10,4 +10,5 @@ This is the bleeding-edge changelog since version 2026.07, for **pre-release 202
 ### Build commands
 
 - queued build cancellation and rejection can apply yardmaps after their existing rectangular checks, allowing the same compatible overlaps as sequential construction. Set `construction.useYardmapsForQueuedBuildOverlap = true` in `gamedata/modrules.lua` to enable yardmap-aware queue overlap; the default remains the previous rectangle-only behavior.
+- queued build outlines that would be removed by the previewed placement use the configurable `buildBoxCancel` command color, which defaults to red.
 - add `Spring.TestBuildOrderOverlap(queuedBuild, proposedBuild) → boolean overlaps, boolean cancels` to test a queued build against a proposed build. `overlaps` reports any conflict under the queued-build yardmap rules; `cancels` reports that the conflict is also inside the cancellation rectangle. Each build is `{unitDefID, x, y, z, facing}`.

rts/Game/UI/CommandColors.cpp
@@ -79,6 +79,7 @@ CCommandColors::CCommandColors()
 	SETUP_COLOR(rangeInterceptorOff, 0.0f, 0.0f, 0.0f, 0.7f);
 	SETUP_COLOR(unitBox,             0.0f, 1.0f, 0.0f, 0.9f);
 	SETUP_COLOR(buildBox,            0.0f, 1.0f, 0.0f, 0.9f);
+	SETUP_COLOR(buildBoxCancel,     1.0f, 0.0f, 0.0f, 0.9f);
 	SETUP_COLOR(allyBuildBox,        0.8f, 0.8f, 0.2f, 0.9f);
 	SETUP_COLOR(mouseBox,            1.0f, 1.0f, 1.0f, 0.9f);
 }
@@ -332,4 +333,3 @@ bool CCommandColors::LoadConfigFromString(const std::string& cfg)
 
 	return true;
 }
-

rts/Game/UI/GuiHandler.cpp
@@ -23,6 +23,7 @@
 #include "Map/MapInfo.h"
 #include "Map/MetalMap.h"
 #include "Map/ReadMap.h"
+#include "Rendering/CommandDrawer.h"
 #include "Rendering/Fonts/glFont.h"
 #include "Rendering/IconHandler.h"
 #include "Rendering/Units/UnitDrawer.h"
@@ -32,8 +33,10 @@
 #include "Rendering/Textures/NamedTextures.h"
 #include "Sim/Features/Feature.h"
 #include "Sim/Misc/LosHandler.h"
+#include "Sim/Misc/TeamHandler.h"
 #include "Sim/Units/CommandAI/CommandAI.h"
 #include "Sim/Units/CommandAI/BuilderCAI.h"
+#include "Sim/Units/QueuedBuildOverlap.h"
 #include "Sim/Units/UnitDefHandler.h"
 #include "Sim/Units/Unit.h"
 #include "Sim/Units/UnitHandler.h"
@@ -2464,22 +2467,68 @@ Command CGuiHandler::GetCommand(int mouseX, int mouseY, int buttonHint, bool pre
 	return Command(CMD_STOP);
 }
 
-
-
-static bool WouldCancelAnyQueued(const BuildInfo& b)
+static bool WouldCancelAnyQueued(const BuildInfo& buildInfo)
 {
 	RECOIL_DETAILED_TRACY_ZONE;
-	const Command c = b.CreateCommand();
+	const Command command = buildInfo.CreateCommand();
 
 	for (const int unitID: selectedUnitsHandler.selectedUnits) {
-		const CUnit* u = unitHandler.GetUnit(unitID);
+		const CUnit* unit = unitHandler.GetUnit(unitID);
 
-		if (u->commandAI->WillCancelQueued(c))
+		if (unit->commandAI->WillCancelQueued(command))
 			return true;
-
 	}
 
 	return false;
+}
+
+using CancelledBuildCommandTags = spring::unordered_map<int, spring::unordered_set<unsigned int>>;
+
+static void DrawQueuedBuildSquares(const CancelledBuildCommandTags& cancelledCommandTags)
+{
+	if (cmdColors.buildBox[3] <= 0.0f || selectedUnitsHandler.selectedUnits.empty())
+		return;
+
+	const bool drawOnShift = cmdColors.BuildBoxesOnShift() && KeyInput::GetKeyModState(KMOD_SHIFT);
+	const bool activeBuildCommand =
+		guihandler->inCommand >= 0 &&
+		guihandler->inCommand < int(guihandler->commands.size()) &&
+		guihandler->commands[guihandler->inCommand].id < 0;
+
+	if (!drawOnShift && !activeBuildCommand)
+		return;
+
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(cmdColors.UnitBoxLineWidth());
+
+	static const spring::unordered_set<unsigned int> noCancelledCommands;
+	for (const auto& [builderID, builderCAI]: unitHandler.GetBuilderCAIs()) {
+		const CUnit* builder = builderCAI->owner;
+		const float* buildColor = nullptr;
+
+		if (builder->team == gu->myTeam) {
+			buildColor = cmdColors.buildBox;
+		} else if (teamHandler.AlliedTeams(builder->team, gu->myTeam)) {
+			buildColor = cmdColors.allyBuildBox;
+		} else {
+			continue;
+		}
+
+		const auto cancelledIt = cancelledCommandTags.find(builderID);
+		const auto& cancelled = (cancelledIt != cancelledCommandTags.end()) ? cancelledIt->second : noCancelledCommands;
+		glColor4fv(buildColor);
+		commandDrawer->DrawQuedBuildingSquares(builderCAI, cancelled, cmdColors.buildBoxCancel);
+	}
+
+	glLineWidth(1.0f);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glDisable(GL_BLEND);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
 }
 
 static void FillRowOfBuildPos(const BuildInfo& startInfo, float x, float z, float xstep, float zstep, int n, int facing, bool nocancel, std::vector<BuildInfo>& ret)
@@ -3543,6 +3592,8 @@ static inline void DrawWeaponArc(const CUnit* unit)
 void CGuiHandler::DrawMapStuff(bool onMiniMap)
 {
 	RECOIL_DETAILED_TRACY_ZONE;
+	CancelledBuildCommandTags cancelledBuildCommandTags;
+
 	if (!onMiniMap) {
 		glEnable(GL_DEPTH_TEST);
 		glDepthMask(GL_FALSE);
@@ -3835,7 +3886,6 @@ void CGuiHandler::DrawMapStuff(bool onMiniMap)
 
 				for (const BuildInfo& bi: buildInfos) {
 					const float3& buildPos = bi.pos;
-
 					DrawUnitDefRanges(nullptr, buildeeDef, buildPos);
 
 					// draw (primary) weapon range
@@ -3864,9 +3914,21 @@ void CGuiHandler::DrawMapStuff(bool onMiniMap)
 						for (const int unitID: selectedUnitsHandler.selectedUnits) {
 							const CUnit* su = unitHandler.GetUnit(unitID);
 							const CCommandAI* cai = su->commandAI;
+							const CBuilderCAI* builderCAI = dynamic_cast<const CBuilderCAI*>(cai);
+							const std::vector<Command> overlapCommands = cai->GetOverlapQueued(c);
 
-							for (const Command& cmd: cai->GetOverlapQueued(c)) {
+							for (const Command& cmd: overlapCommands) {
 								buildCommands.push_back(cmd);
+
+								const bool activeBuild =
+									builderCAI != nullptr && builderCAI->HasCurrentBuild() &&
+									!cai->commandQue.empty() && cmd.GetTag() == cai->commandQue.front().GetTag();
+								if (activeBuild)
+									continue;
+
+								const BuildInfo queuedBuild(cmd);
+								if (QueuedBuildOverlap::IsInsideCancellationRectangle(queuedBuild, bi))
+									cancelledBuildCommandTags[unitID].insert(cmd.GetTag());
 							}
 						}
 					}
@@ -3933,6 +3995,8 @@ void CGuiHandler::DrawMapStuff(bool onMiniMap)
 			}
 		}
 	}
+
+	DrawQueuedBuildSquares(cancelledBuildCommandTags);
 
 	glLineWidth(1.0f);
 
@@ -4434,4 +4498,3 @@ void CGuiHandler::DrawSelectCircle(const float3& pos, float radius,
 
 	glEnable(GL_FOG);
 }
-